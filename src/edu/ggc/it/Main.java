9a491af8e62997e9ac943a78ab92e1778d85e706
Tacksoo Im <tacksoo.im@gmail.com>
2011-05-16 00:42:33 -0400

    added an new activity: credits

--- src/edu/ggc/it/Main.java
+++ src/edu/ggc/it/Main.java
@@ -1,10 +1,13 @@
 package edu.ggc.it;
 
-
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -32,7 +35,31 @@ public class Main extends Activity  {
 
 	}
 
-
+	/** Called when user presses Menu key */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.app_menu, menu);
+	    return true;
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.welcome:
+	        //newGame();
+	        return true;
+	    case R.id.credits:
+	        Intent myIntent = new Intent(Main.this,Credits.class);
+	        Main.this.startActivity(myIntent);
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+	
 	public class MyListener implements OnClickListener {
 		public void onClick(View view) {
 			timeButton.setText("Hello Android");
