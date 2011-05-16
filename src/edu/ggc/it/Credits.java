1b2ace77126f1448e2c478d143993150cfa4707c
Tacksoo Im <tacksoo.im@gmail.com>
2011-05-16 00:47:34 -0400

    new resources

---
+++ src/edu/ggc/it/Credits.java
@@ -0,0 +1,22 @@
+package edu.ggc.it;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Credits extends Activity {
+
+	private TextView myText;
+
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		// will throw exception if you do not call this
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.credits);
+		
+		myText = (TextView) findViewById(R.id.creditsTextView);
+	}
+	
+}
