5e210f5632534890063d468a12426887d7c80250
Jacob <jsmallwo@ggc.edu>
2013-02-11 13:35:48 -0500

    Added skeleton for SetupActivity class

---
+++ src/edu/ggc/it/love/SetupActivity.java
@@ -0,0 +1,14 @@
+package edu.ggc.it.love;
+
+import android.app.Activity;
+
+/**
+ * This activity prompts the user for their profile information
+ * on their first run of the activity, or forward them to the
+ * search page if the information has already been entered.
+ * @author Jacob
+ *
+ */
+public class SetupActivity extends Activity{
+	
+}
