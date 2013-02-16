4c00caae313241acbabf3581e7989a1de7a5dcc2
JohnnyLynch <alynch@ggc.edu>
2013-02-16 12:05:39 -0500

    This is a major commit the application is now receiving GPS data(after about 60-90 seconds from the start of the Activity).
    	TODO
    	- find a way to start GPS updates faster
    	- find a way to display a PDF 
    	- find a way to sync up GPS data with the PDF
    	- Enable zooming with the PDF
    	- fine a way to move and image over the PDF to show user location

--- src/edu/ggc/it/map/MapActivity.java
+++ src/edu/ggc/it/map/MapActivity.java
@@ -18,11 +18,18 @@ import edu.ggc.it.R;
 public class MapActivity extends Activity {
 	
 	/**
-	 *  MapActivity has an image of GGC for the user to see.
-	 *  TODO add GPS Object to this. That will handle all of
-	 *  GPS data and return the latitude and longitude of the phone,
-	 *  and then show a representation of the location of the user on the
-	 *  map image of GGC.
+	 *  MapActivity has an image of GGC for the user to use for navigation.
+	 *  
+	 *  As of this commit this application is getting PGS data
+	 *  Although it does take about 60 to 90 seconds be able to get 
+	 *  the GPS date. This is an issue that will be worked out in 
+	 *  future commits. 
+	 *  
+	 *  TODO
+	 *  - find a way to display a PDF 
+	 *  - find a way to sink up GPS data with the PDF
+	 *  - Enable zooming with the PDF
+	 *  - fine a way to move and image of the PDF to show user location
 	 */
 	
 	private TextView textViewGPSOutput;
@@ -35,27 +42,35 @@ public class MapActivity extends Activity {
 		setContentView(R.layout.map_activity);
 		
 		LocationManager locationManager = (LocationManager) this.getSystemService(context.LOCATION_SERVICE);
-				
-		LocationProvider provider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
 		
 	    final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
 	    if (!gpsEnabled) {
-	    	
-	    	Toast.makeText(context, "Your need to enable the GPS service.", Toast.LENGTH_LONG);
+	    	Toast.makeText(context, "GPS service is not Enabled.", Toast.LENGTH_LONG).show();
 	    	//DOTO I need to look up the process the user has to do to be able too fix this problem. 
+	    	enableLocationSettings();
+	    	
 	    }
 	    
+	    GGCLocationListener ggcLocationListener = new GGCLocationListener();
 	    
 	    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-	            10000,          // 10-second interval.
+	            1000,          // 10-second interval.
 	            10,             // 10 meters.
 	            new GGCLocationListener());
-	    	    		
+	    
+	    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+				
+		LocationProvider locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+		
+		Toast.makeText(context, locationProvider.toString(), Toast.LENGTH_LONG).show();
+	    
 	    textViewGPSOutput = (TextView) findViewById(R.id.textViewGPSOutPut);
 	  	
-	  	textViewGPSOutput.setText("0987654321\n1234567890");	
-		
+	  	textViewGPSOutput.setText("0987654321\n1234567890");
+	  	
+    	//Toast.makeText(context, "TEST", Toast.LENGTH_LONG).show();
+
 	}
 	
 	private void enableLocationSettings() {
@@ -63,11 +78,10 @@ public class MapActivity extends Activity {
 	    startActivity(settingsIntent);
 	}
 	
-	
 	/**
-	 * @author andrew
-	 * It is unknown at this time if GGCLocationListener is receiving location updates or not 
-	 * I will be getting a device to test this feature soon.
+	 * @author Andrew
+	 * GGCLocatoinListener is working but it takes about a 60 to 90 seconds to get the first 
+	 * GPS updates. This issue will be dealt with in future commits.
 	 */
 	public class GGCLocationListener implements LocationListener {
 
@@ -75,6 +89,8 @@ public class MapActivity extends Activity {
 	    public void onLocationChanged(Location location) {
 	    	
 	    	textViewGPSOutput.setText(location.getLatitude() + ", " + location.getLongitude());
+	    	
+	    	//Toast.makeText(context, "GPS IS WORKIN!!!", Toast.LENGTH_LONG).show();
 	    	   	
 	        //Message.obtain(mHandler, UPDATE_LATLNG,location.getLatitude() + ", " + location.getLongitude()).sendToTarget();
 
