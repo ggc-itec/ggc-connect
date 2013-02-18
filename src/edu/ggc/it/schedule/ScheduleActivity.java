6c2b29933578bab7891d6a16e0ee8f20e7c96e93
rramsaro <rramsaro@ggc.edu>
2013-02-18 16:11:54 -0500

    The "no" option on dialog box goes back to main activity.

--- src/edu/ggc/it/schedule/ScheduleActivity.java
+++ src/edu/ggc/it/schedule/ScheduleActivity.java
@@ -37,8 +37,7 @@ public class ScheduleActivity extends Activity {
 	     })
 	    .setNegativeButton("No", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int which) { 
-	        	Toast.makeText(scheduleContext, "Go back to main menu", Toast.LENGTH_LONG)
-				.show();
+	        	finish();
 	        }
 	     })
 	     .show();
