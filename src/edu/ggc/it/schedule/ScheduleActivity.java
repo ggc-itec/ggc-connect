78ca270d80a5b80b23e3dbb34650ab376da6c31f
rramsaro <rramsaro@ggc.edu>
2013-02-18 13:41:20 -0500

    Added dialog thing.

--- src/edu/ggc/it/schedule/ScheduleActivity.java
+++ src/edu/ggc/it/schedule/ScheduleActivity.java
@@ -6,8 +6,8 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,9 +19,6 @@ public class ScheduleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_schedule);
-		
-		/*scheduleButton = (Button) findViewById(R.id.add_schedule_item_button);
-		scheduleButton.setOnClickListener(new ScheduleListener());*/
 		
 	}
 
@@ -62,10 +59,8 @@ public class ScheduleActivity extends Activity {
 		}
 		
 	}
-	
-	@SuppressLint("NewApi")
+	@SuppressLint("ValidFragment")
 	public class AddScheduleItemDialog extends DialogFragment {
-	    @SuppressLint("NewApi")
 		@Override
 	    public Dialog onCreateDialog(Bundle savedInstanceState) {
 	        // Use the Builder class for convenient dialog construction
