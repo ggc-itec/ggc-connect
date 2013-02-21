c0a51d7ba231e62a6f012236dc5d14a2679ec946
dewguzzler <smartone514@gmail.com>
2013-02-21 16:45:43 -0500

    After much fighting successfully added listener to departments, now will open webview with corresponding department views

--- src/edu/ggc/it/directory/DirectoryActivity.java
+++ src/edu/ggc/it/directory/DirectoryActivity.java
@@ -8,16 +8,20 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
 
 public class DirectoryActivity extends Activity {
 	
 	private TabHost tabHost;
 	private Intent intent;
+	private Intent intent2;
 	private ListView list;
 	public final static String EXTRA_MESSAGE = "edu.ggc.it.directory.MESSAGE";
 	public final static String lastName = "edu.ggc.it.directory.MESSAGE";
@@ -27,12 +31,43 @@ public class DirectoryActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		intent = new Intent().setClass(this, DirectoryActivity.class);
 		setContentView(R.layout.activity_ggcdirectory);
+		intent2 = new Intent(this, DirectorySearchWebView.class);
 		list = (ListView) findViewById(R.id.directory_listView);
 		list.setAdapter(new ArrayAdapter<String>(this,
 		       android.R.layout.simple_list_item_1, getResources()
 		         .getStringArray(R.array.parent_directories)));
-		 
-
+		list.setOnItemClickListener(new OnItemClickListener(){
+	         public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+	        	 String wholeUrl;
+	        	 if(position == 0){
+	        		 wholeUrl = "http://www.ggc.edu/admissions/meet-our-staff/";
+	        		 intent2.putExtra(EXTRA_MESSAGE, wholeUrl);
+	        		 startActivity(intent2);
+	        	 }
+	        	 if(position == 1){
+	        		 wholeUrl = "http://www.ggc.edu/academics/schools/school-of-business/meet-our-faculty-and-staff/index.html";
+	        		 intent2.putExtra(EXTRA_MESSAGE, wholeUrl);
+	        		 startActivity(intent2);
+	        	 }
+	        	 if(position == 2){
+	        		 wholeUrl = "http://www.ggc.edu/academics/schools/school-of-education/meet-the-faculty-and-staff/index.html";
+	        		 intent2.putExtra(EXTRA_MESSAGE, wholeUrl);
+	        		 startActivity(intent2);
+	        	 }
+	        	 if(position == 3){
+	        		 wholeUrl = "http://www.ggc.edu/academics/schools/school-of-liberal-arts/meet%20the%20facultyand%20staff/index.html";
+	        		 intent2.putExtra(EXTRA_MESSAGE, wholeUrl);
+	        		 startActivity(intent2);
+	        	 }
+	        	 if(position == 4){
+	        		 wholeUrl = "http://www.ggc.edu/academics/schools/school-of-science-and-technology/meet-faculty-and-staff.html";
+	        		 intent2.putExtra(EXTRA_MESSAGE, wholeUrl);
+	        		 startActivity(intent2);
+	        	 }
+	        	 
+	         }
+	        }
+	        );
         tabHost = (TabHost) findViewById(R.id.tabHost);
 		tabHost.setup();
 		TabSpec spec1 = tabHost.newTabSpec("First Tab");
