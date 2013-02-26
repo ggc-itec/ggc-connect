2f551e9cb4c417210e8f8856a66217cbc6065572
thaives <phamhuynhthai@gmail.com>
2013-02-26 18:25:26 -0500

    clean the project, changed the GGC map, and added current position on map

--- src/edu/ggc/it/direction/DirectionActivity.java
+++ src/edu/ggc/it/direction/DirectionActivity.java
@@ -1,16 +1,11 @@
 package edu.ggc.it.direction;
 
-//import android.location.Location;
-//import android.location.LocationManager;
 import edu.ggc.it.R;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -19,60 +14,77 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
-
+/**
+ * This class is an activity class
+ * @author Thai Pham
+ * @version 0.1
+ *
+ */
 public class DirectionActivity extends Activity {
-	
-	//private Button findButton;
+	//This aims to get the width of the image view 
+	private int imgWidth;
+	//This aims to get the height of the image
+	private int imgHeight;
+	//This aims to get the height of the image view 
+	private int imgViewHeight;
+	//This aims to get the left of the image view 
+	private int imgLeft;
+	//This aims to get the top of the image view 
+	private int imgViewTop;
+	//This aims to get the space between the top of View and top of image
+	private int imgPadding;
+	//Condition value allows program get position of image one time
+	private boolean save = false;
+	//Create a imageview for the GGC map
 	private ImageView img;
-
+	//Create a imageview for the current position on map
+	private ImageView img1;
+	//Create new context for activity
 	private Context myContext;
-
+	//Create a textview to display instructions to users
 	private TextView instructionText;
-	
-	//private LocationManager lm;
-	//private double latitude;
-	//private double longitude;
-	//private Location loc;
-
+	//Create location manager 
+	private LocationManager lm;
+	//This will get the user's latitude
+	private double latitude;
+	//This will get the user's longitude
+	private double longitude;
+	//Create new location
+	private Location loc;
+	//Create a location list that holds list of places in GGC
 	private LocationList myLocationList;
-	
+	//Create a new spinner to display list of places in GGC 
 	private Spinner spin;
+	//This will get the item that users choose from place's list of spinner
 	private String spin_val;
-	@Override
+	/**
+	 * @Override
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_direction);
 		myContext = this;
+		//Init locationlist
 		myLocationList = new LocationList();
-		
+		//Create new listenner for spinner
 		myItemSelectedListenner mySelectedListenner = new myItemSelectedListenner();
 		spin = (Spinner) findViewById(R.id.spinnerText);
 		spin.setOnItemSelectedListener(mySelectedListenner);
-
+		
 		instructionText = (TextView) findViewById(R.id.instruction_text);
 		img = (ImageView) findViewById(R.id.imageMap);
-
-		//-------------------------------------
-		int w = img.getWidth(), h = img.getHeight();
+		img1=(ImageView) findViewById(R.id.imageYou);
 		
-		Paint currentPaint = new Paint();
-		currentPaint.setDither(true);
-        currentPaint.setColor(0xFF00CC00);  // alpha.r.g.b
-        currentPaint.setStyle(Paint.Style.STROKE);
-        currentPaint.setStrokeJoin(Paint.Join.ROUND);
-        currentPaint.setStrokeCap(Paint.Cap.ROUND);
-        currentPaint.setStrokeWidth(2);
-
-		Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
-		Bitmap bmp = Bitmap.createBitmap(w, h, conf); // this creates a MUTABLE bitmap
-		Canvas canvas = new Canvas(bmp);
-		canvas.drawRect(100, 100, 200, 200, currentPaint);
-		
-		//--------------------------------------
+		//Require real device has GPS
 		//lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 		//latitude = lm.getLastKnownLocation(lm.GPS_PROVIDER).getLatitude();
 		//longitude = lm.getLastKnownLocation(lm.GPS_PROVIDER).getLongitude();
-
+		
+		//Test without real device has GPS
+		latitude = 33.98474;
+		longitude =  -84.00265;
+		
+		//Create ArrayAdapter for spinner
 		ArrayAdapter<String> spin_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, myLocationList.getNameList());
 	      // setting adapter to spinner
 	    spin.setAdapter(spin_adapter);
@@ -84,25 +96,51 @@ public class DirectionActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_direction, menu);
 		return true;
 	}
+	
+	/**
+	 * @Override
+	 *
+	 */
 	public class myItemSelectedListenner implements OnItemSelectedListener{
 
-		@Override
 		public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
-			// TODO Auto-generated method stub
 			spin_val = myLocationList.getInstruction(position);
 			String bld = myLocationList.getBuilding(position);
+			if (!save){//allow device get position one time
+				imgLeft = img.getLeft();
+				imgViewTop = img.getTop();
+				imgWidth = img.getWidth();
+				//Find the relative height of image on its width
+				imgHeight = imgWidth*913/1188;
+				imgViewHeight = img.getHeight();
+				//Find the space between the top of View and Image
+				imgPadding = (imgViewHeight - imgHeight)/2;
+				//Update the top for the image
+				imgViewTop = imgViewTop + imgPadding;
+				//Do not to re-get above data
+				save = true;
+			}
+			//Set the x for the current user's position on Map(-84.01209 to -83.99772)
+			img1.setX((float) (imgLeft+((longitude + 84.01209)*100000*imgWidth/1437)));
+			//Set the y for the current user's position on Map(33.98565 to 33.97652)
+			img1.setY((float) (imgViewTop-30+((33.98565 - latitude)*100000*imgHeight/913)));
+			
+			//Check and run these lines when user click nothing or the first row in the place's list
 			if(bld.length()==0){
+				img.setImageResource(R.drawable.thai_ggc_map);	
+				img1.setImageResource(R.drawable.you);
+				//Set the current position of user on Map
+				img1.setVisibility(View.VISIBLE);
+				//Display instruction to the textview 
 				instructionText.setText(spin_val);
-				img.setImageResource(R.drawable.thai_ggc_map);
 			}
-			else{
-			instructionText.setText("It is located on building "+ bld
-					//Line below just used for test
-					//+"\n Latitude: "+latitude +"\n Longitude: "+longitude
-					+ "\nInstruction: \n"+"    "+spin_val
-					+ "\nHere is building "+bld+" map");
+			else{//Run these lines when users click on any item on the list of spinner
+				img1.setVisibility(View.INVISIBLE);
+				instructionText.setText("It is located on building "+ bld
+						+ "\nInstruction: \n"+"    "+spin_val
+						+ "\nHere is building "+bld+" map");
 			}
-			//bld = myLocationList.getBuilding(position);
+			//Depend on building where the instruction points to, these lines will show the appropriate map
 			if (bld.equals("A")){
 				img.setImageResource(R.drawable.building_a);
 			}else if(bld.equals("B")){
@@ -129,11 +167,5 @@ public class DirectionActivity extends Activity {
 			// TODO Auto-generated method stub	
 		}
 		
-	}
-	public class MyListener implements OnClickListener {
-		@Override
-		public void onClick(View view) {
-			
-		}
 	}
 }
--- src/edu/ggc/it/direction/LocationList.java
+++ src/edu/ggc/it/direction/LocationList.java
@@ -1,9 +1,18 @@
 package edu.ggc.it.direction;
 
 import java.util.ArrayList;
-
+/**
+ * This class aims to create a list of location and feed for the spinner.
+ * @author Thai Pham
+ * @version 0.1
+ *
+ */
 public class LocationList {
+	//Create a new arraylist
 	private ArrayList<Location> locationList;
+	/**
+	 * Constructor to create a new LocationList
+	 */
 	public LocationList(){	
 		locationList = new ArrayList<Location>();
 		locationList.add(new Location("Please choose a place you are looking! ","","Welcome to GGC!"));
@@ -20,14 +29,27 @@ public class LocationList {
 		//Add some more here...
 	}
 	
+	/**
+	 * This method is to get the size of arraylist
+	 * @return
+	 */
 	public int getSize(){
 		return locationList.size();
 	}
 	
+	/**
+	 * This method is to get the name of a location in location list
+	 * @param i: position of element want to get
+	 * @return name of the location which has position i in the location list
+	 */
 	public String getElement(int i){
 		return locationList.get(i).getName();
 	}
 	
+	/**
+	 * This method is to get the arraylist of only the name of location, to display in the spinner
+	 * @return list of locations' name
+	 */
 	public ArrayList<String> getNameList(){
 		ArrayList<String> nameList = new ArrayList<String>();
 		for(int i = 0; i < locationList.size(); i++){
@@ -36,10 +58,20 @@ public class LocationList {
 		return nameList;
 	}
 	
+	/**
+	 * This method is to get the building name of a location in the location list
+	 * @param i: position of a place want to get the building name
+	 * @return name of the building where the place is on
+	 */
 	public String getBuilding(int i){
 		return locationList.get(i).getBuilding();
 	}
 	
+	/**
+	 * This method is to get the instruction to find the place on map
+	 * @param i: position of the location in location list
+	 * @return instruction for the place which has position is i
+	 */
 	public String getInstruction(int i){
 		return locationList.get(i).getInstruction();
 	}
