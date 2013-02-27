7fbda753b9c2ee9edf6ac5a35f8742fc0d453799
Leo Hernandez <lhernand@ggc.edu>
2013-02-27 12:21:48 -0500

    files updated

--- src/edu/ggc/it/gym/GroupsActivity.java
+++ src/edu/ggc/it/gym/GroupsActivity.java
@@ -7,12 +7,19 @@ import edu.ggc.it.R.menu;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 public class GroupsActivity extends Activity {
@@ -29,12 +36,15 @@ public class GroupsActivity extends Activity {
 	private Button yoga;
 	private Button interval3;
 	private Context myContext;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_groups);
 
 		myContext = this;
+
 
 		muscle1 = (Button) findViewById(R.id.muscle1);
 		muscle1.setOnClickListener(new ButtonListener());
@@ -78,30 +88,125 @@ public class GroupsActivity extends Activity {
 	}
 	public class ButtonListener implements OnClickListener {
 		public void onClick(View view) {
-			
+
 			if (view.getId() == R.id.muscle1 || view.getId() == R.id.muscle2){
-				Toast.makeText(myContext, "Tone and build muscle mass utilizing different resistance tools.", Toast.LENGTH_LONG)
-				.show();
+				//Toast.makeText(myContext, "Tone and build muscle mass utilizing different resistance tools.", Toast.LENGTH_LONG)
+				//.show();
+				muscleDesc();				
 			}
 			else if (view.getId() == R.id.boot1 || view.getId() == R.id.boot2){
-			
-				Toast.makeText(myContext, "Boot camp is a group exercise class that mixes traditional calisthenics and body weight exercises with interval training and strength training.", Toast.LENGTH_LONG).show();
+				bootDesc();
+				//Toast.makeText(myContext, "Boot camp is a group exercise class that mixes traditional calisthenics and body weight exercises with interval training and strength training.", Toast.LENGTH_LONG).show();
 			}
 			else if (view.getId() == R.id.yogaletes){
-				Toast.makeText(myContext, "Combines Yoga and Pilates.", Toast.LENGTH_LONG).show();
+				//Toast.makeText(myContext, "Combines Yoga and Pilates.", Toast.LENGTH_LONG).show();
+				yogaletesDesc();
 			}
 			else if (view.getId() == R.id.yoga){
-				
-				Toast.makeText(myContext, "Appropriate for the beginner who wants to build a strong foundation of basic Yoga postures and breathing techniques, as well as for the practitioner who wants to refine and master the fundamentals. It is an invitation to stretch, relax, unwind and de-stress.", Toast.LENGTH_LONG).show();
+				yogaDesc();
+				//Toast.makeText(myContext, "Appropriate for the beginner who wants to build a strong foundation of basic Yoga postures and breathing techniques, as well as for the practitioner who wants to refine and master the fundamentals. It is an invitation to stretch, relax, unwind and de-stress.", Toast.LENGTH_LONG).show();
 			}
 			else if (view.getId() == R.id.interval1 || view.getId() == R.id.interval2 || view.getId() == R.id.interval3){
-				Toast.makeText(myContext, "A great cardio & strength training workout.", Toast.LENGTH_LONG).show();
+				intervalDesc();
+				//Toast.makeText(myContext, "A great cardio & strength training workout.", Toast.LENGTH_LONG).show();
 			}
-			else if (view.getId() == R.id.homeButton) {
-				startActivity(new Intent (myContext, Main.class));
+			else if (view.getId() == R.id.kickboxing) {
+				kickDesc();
+				//Toast.makeText(myContext, "Martial arts-inspired interval workout that combines kickboxing with athletic drills.", Toast.LENGTH_LONG).show();
 			}
+			else if (view.getId() == R.id.strength1) {
+				strengthDesc();
+				//Toast.makeText(myContext, "This interval training routine blends strength and cardio alternating circuits using body weight, a variety of equipment and partner work. The circuit training workout is loaded with functional exercises designed to give you the ultimate challenge.", Toast.LENGTH_LONG).show();
+			}
+
 		}
 
 	}
 
+	private void showAddScheduleItemDialog() {
+		new AlertDialog.Builder(this)
+		// .setTitle("No Classes Found")
+		.setMessage(R.string.schedule_no_classes_popup)
+
+
+
+		.show();
+	}
+	public void muscleDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+		// set title
+		//alertDialogBuilder.setTitle("Your Title");
+
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("Tone and build muscle mass utilizing different resistance tools.");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+	}
+	public void bootDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("Boot camp is a group exercise class that mixes traditional calisthenics and body weight exercises with interval training and strength training.");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+	}
+	public void yogaletesDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("Combines Yoga and Pilates");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+	}
+	public void yogaDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("Appropriate for the beginner who wants to build a strong foundation of basic Yoga postures and breathing techniques, as well as for the practitioner who wants to refine and master the fundamentals. It is an invitation to stretch, relax, unwind and de-stress.");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+	}
+	
+	public void intervalDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("A great cardio & strength training workout.");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+	}
+	public void kickDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("Martial arts-inspired interval workout that combines kickboxing with athletic drills.");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+	}
+	public void strengthDesc() {
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		// set dialog message;
+		alertDialogBuilder
+		.setMessage("This interval training routine blends strength and cardio alternating circuits using body weight, a variety of equipment and partner work. The circuit training workout is loaded with functional exercises designed to give you the ultimate challenge.");
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+	}
+	
 }
