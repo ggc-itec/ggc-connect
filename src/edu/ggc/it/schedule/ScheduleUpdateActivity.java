4a5ba3ae197909956b76aab629ee8ebcf70bf264
rramsaro <rramsaro@ggc.edu>
2013-03-01 13:25:09 -0500

    Trying to get start/end time buttons to work.

--- src/edu/ggc/it/schedule/ScheduleUpdateActivity.java
+++ src/edu/ggc/it/schedule/ScheduleUpdateActivity.java
@@ -1,21 +1,20 @@
 package edu.ggc.it.schedule;
 
-import android.animation.TimeAnimator;
-import android.animation.TimeAnimator.TimeListener;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 import edu.ggc.it.R;
 
 public class ScheduleUpdateActivity extends Activity {
@@ -42,6 +41,9 @@ public class ScheduleUpdateActivity extends Activity {
 			chkFriday, chkSaturday;
 	private Spinner spnBuildingLocation;
 	private EditText txtRoomLocation;
+	
+	private int mHour;
+	private int mMinute;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -89,12 +91,40 @@ public class ScheduleUpdateActivity extends Activity {
 			} else if (view.getId() == R.id.btn_schedule_update_submit) {
 				addClass();
 			} else if (view.getId() == R.id.btn_schedule_update_start_time) {
-				//TODO: Show time picker dialog
-				showDialog(R.id.schedule_start_time_picker);
+				showTimePicker();
 			} else if (view.getId() == R.id.btn_schedule_update_end_time) {
-				showDialog(R.id.schedule_end_time_picker);
+				showTimePicker();
 			}
 		}
+	}
+	
+	private void showTimePicker() {
+		/** Creating a bundle object to pass currently set time to the fragment */
+		Bundle b = new Bundle();
+		
+		/** Adding currently set hour to bundle object */
+		b.putInt("set_hour", mHour);
+		
+		/** Adding currently set minute to bundle object */
+		b.putInt("set_minute", mMinute);
+		
+		/** Instantiating TimePickerDialogFragment */
+		Fragment timePicker = new Fragment();
+		
+		/** Setting the bundle object on timepicker fragment */
+		timePicker.setArguments(b);				
+		
+		/** Getting fragment manger for this activity */
+		FragmentManager fm = getFragmentManager();				
+		
+		/** Starting a fragment transaction */
+		FragmentTransaction ft = fm.beginTransaction();
+		
+		/** Adding the fragment object to the fragment transaction */
+		ft.add(timePicker, "time_picker");
+		
+		/** Opening the TimePicker fragment */
+		ft.commit();
 	}
 	
 	/*@Override
@@ -144,6 +174,23 @@ public class ScheduleUpdateActivity extends Activity {
 	protected void onDestroy() {
 		super.onDestroy();
 		database.close();
+	}
+	
+	public class ScheduleHandler extends Handler {
+		@Override
+        public void handleMessage(Message m){   
+        	/** Creating a bundle object to pass currently set Time to the fragment */
+        	Bundle b = m.getData();
+        	
+        	/** Getting the Hour of day from bundle */
+    		mHour = b.getInt("set_hour");
+    		
+    		/** Getting the Minute of the hour from bundle */
+    		mMinute = b.getInt("set_minute");
+    		
+    		/** Displaying a short time message containing time set by Time picker dialog fragment */
+    		Toast.makeText(getBaseContext(), b.getString("set_time"), Toast.LENGTH_SHORT).show();
+        }
 	}
 
 }
