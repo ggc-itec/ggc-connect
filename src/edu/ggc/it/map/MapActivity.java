56c2210192a7ba879b90437f202f5c632d645192
JohnnyLynch <alynch@ggc.edu>
2013-03-04 13:36:38 -0500

    State save so I can change branches

--- src/edu/ggc/it/map/MapActivity.java
+++ src/edu/ggc/it/map/MapActivity.java
@@ -1,170 +1,53 @@
 package edu.ggc.it.map;
 
-import javax.crypto.spec.IvParameterSpec;
-import javax.xml.xpath.XPath;
+import java.util.HashMap;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.PointF;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
-import android.opengl.Visibility;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.renderscript.Matrix2f;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 import edu.ggc.it.R;
-/*
-//import android.support.v4.widget.ImageView;
-Log.d("LOCAL", "view Hights: "+view.getMeasuredHeight()+" view Width :"+view.getMeasuredWidth()+" imageViewBackGround scaleX: "+imageViewBackGround.getScaleX()
-		+ " imageViewBackGround scaleY: "+imageViewBackGround.getScaleY()+" BG ScrollX: "+ imageViewBackGround.getScrollX()+" BG ScrollY: "+imageViewBackGround.getScrollX()
-		+"BG Width: "+ imageViewBackGround.getWidth()+ " BG Hight: " +imageViewBackGround.getHeight()+ "BG X: "+imageViewBackGround.getY()+" BG Y: "+imageViewBackGround.getY() );
-*/
 
 public class MapActivity extends Activity {
 	
-
-	Context context = this;
-	LocationManager locationManager;
-	GGCLocationListener ggcLocactionListener;
-	ImageButton imageButtonABuilding;
-	ImageButton imageButtonBBuilding;
-	ImageButton imageButtonCBuilding;
-	ImageButton imageButtonDBuilding;
-	ImageButton imageButtonFBuilding;
-	ImageButton imageButtonLBuilding;
-	ImageButton imageButtonStudentCenter;
-	ImageView redDot;
-	ImageView imageViewBackGround;
-	RelativeLayout relativeLayout;
-	private Matrix matrix = new Matrix();
-	private Matrix oldMatrix = new Matrix();
-	public enum State {
-		NONE, DRAGGING, ZOOMING
-	};
-	private State mode = State.NONE;
-	private PointF start = new PointF();
-	private PointF middle = new PointF();
-	float oldDistance = 1f;
-	double oldXVal;
-	double oldYVal;
-	double[] matrixDataOfBackground;
-	double[] gpsLocation;
-	float[] redDotArray;
-	float[] imageButtonABuildingArray;
-	float[] imageButtonBBuildingArray;
-	float[] imageButtonCBuildingArray;
-	float[] imageButtonDBuildingArray;
-	float[] imageButtonFBuildingArray;
-	float[] imageButtonLBuildingArray;
-	float[] imageButtonStudentCenterArray;
-
-	double yPixVal;
-	double xPixVal;
-	double heightAtCurrentScale;
-	double widthAtCurrentScale;
-
-
-	double oldXScale;
-	double oldYScale;
-
-	Boolean firstRun;
-
+	private Context context = this;
+	private LocationManager locationManager;
+	private GGCLocationListener ggcLocactionListener;
 	
 	/**
 	 *  MapActivity has an image of GGC that helps users know where they are. 
 	 */
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView( R.layout.map_activity);
-		redDot = (ImageView) findViewById(R.id.imageView_redDot);
-		imageViewBackGround = (ImageView) findViewById(R.id.imageView_GGC_Full_Map_Small);
-		imageViewBackGround.setOnTouchListener(new TouchListener());
+		setContentView( new MapView(context));
 		setUpGPS();
-		setUpImageButtons();
-		setImageButtonsLocation();
-		oldXScale=1;
-		oldXScale=1;
-		firstRun =true;
-		matrixDataOfBackground = new double[4];
-		gpsLocation = new double[2];
-		gpsLocation[0] = 0.0;
-		gpsLocation[1] = 0.0;
-		//redDot.setScaleX(imageViewBackGround.getWidth()/32);
-		//redDot.setScaleY(imageViewBackGround.getWidth()/32);
 	}
-	
-	private void setImageButtonsLocation() {
-		imageButtonABuilding.setX(400);
-		imageButtonABuilding.setY(750);
-		imageButtonBBuilding.setX(500);
-		imageButtonBBuilding.setY(460);
-		imageButtonCBuilding.setX(450);
-		imageButtonCBuilding.setY(400);
-		imageButtonStudentCenter.setX(360);
-		imageButtonStudentCenter.setY(423);
-		imageButtonLBuilding.setX(390);
-		imageButtonLBuilding.setY(525);
-		imageButtonFBuilding.setX(450);
-		imageButtonFBuilding.setY(875);
-		imageButtonDBuilding.setX(400);
-		imageButtonDBuilding.setY(950);	
-		redDot.setX(-15);
-		redDot.setY(-14);
-	}
-	
 
-	/**
-	 *  setUpImageButtons sets up all of the ImageButtons, sets their background color to TRANSPARENT, and adds and {@link GGCOnClickListener} to each
-	 *  of the ImageButtons. 
-	 */
-	private void setUpImageButtons() {
-		
-		GGCOnClickListener ggcOnClickListener = new GGCOnClickListener();	
-		//A
-		imageButtonABuilding = (ImageButton) findViewById(R.id.imageButtonABulling);
-		//imageButtonABuilding.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonABuilding.setOnClickListener(ggcOnClickListener);
-		//B
-		imageButtonBBuilding = (ImageButton) findViewById(R.id.imageButtonBBuilding);
-		//imageButtonBBuilding.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonBBuilding.setOnClickListener(ggcOnClickListener);
-		//C
-		imageButtonCBuilding = (ImageButton) findViewById(R.id.imageButtonCBuilding);
-		//imageButtonCBuilding.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonCBuilding.setOnClickListener(ggcOnClickListener);
-		//D
-		imageButtonDBuilding = (ImageButton) findViewById(R.id.imageButtonDBuilding);
-		//imageButtonDBuilding.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonDBuilding.setOnClickListener(ggcOnClickListener);
-		//F
-		imageButtonFBuilding = (ImageButton) findViewById(R.id.imageButtonFBuilding);
-		//imageButtonFBuilding.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonFBuilding.setOnClickListener(ggcOnClickListener);
-		//L
-		imageButtonLBuilding = (ImageButton) findViewById(R.id.imageButtonLBuilding);
-		//imageButtonLBuilding.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonLBuilding.setOnClickListener(ggcOnClickListener);
-		//Student Center
-		imageButtonStudentCenter = (ImageButton) findViewById(R.id.imageButtonStudentCenter);
-		//imageButtonStudentCenter.setBackgroundColor(Color.TRANSPARENT);	
-		imageButtonStudentCenter.setOnClickListener(ggcOnClickListener);
-	}// TODO Go back and have all of the setBackgroundColor s in an method so they can turn on and off.
-	
 	private float spaceBetweenTwoFingers(MotionEvent event) {
 		float x = event.getX(0) - event.getX(1);
 		float y = event.getY(0) - event.getY(1);
@@ -198,87 +81,17 @@ public class MapActivity extends Activity {
 	    locationManager.removeUpdates(ggcLocactionListener);
 	}
 	
-	private class GGCOnClickListener implements OnClickListener{
-
-		@Override
-		public void onClick(View view) {		
-			if(view.getId() == R.id.imageButtonABulling){ 
-				startActivity(new Intent(context, ImageTouchABuildingActivity.class));
-			}else if(view.getId() == R.id.imageButtonBBuilding ){ 
-				startActivity(new Intent(context, ImageTouchBBuildingActivity.class));
-			}else if(view.getId() == R.id.imageButtonCBuilding ){ 
-				startActivity(new Intent(context, ImageTouchCBuildingActivity.class));
-			}else  if(view.getId() == R.id.imageButtonDBuilding ){ 
-				startActivity(new Intent(context, ImageTouchDBuildingActivity.class));
-			}else  if(view.getId() == R.id.imageButtonFBuilding ){ 
-				startActivity(new Intent(context, ImageTouchFBuildingActivity.class));
-			}else  if(view.getId() == R.id.imageButtonLBuilding ){ 
-				startActivity(new Intent(context, ImageTouchLBuildingActivity.class));
-			}else  if(view.getId() == R.id.imageButtonStudentCenter){ 
-				startActivity(new Intent(context, ImageTouchStudentCenterActivity.class));
-			}
-		}		
-		
-	}
-
 	private class GGCLocationListener implements LocationListener{
-
 		
 		@Override
 		public void onLocationChanged(Location location) {
 			double latitude = location.getLatitude();
 			double longitude = location.getLongitude();
-			String lat = latitude+"";
-			lat = lat.substring(0,7);
-			gpsLocation[0] = Double.parseDouble(lat);
-			String lon = longitude+"";
-			lon = lon.substring(0,8);
-			gpsLocation[1] = Double.parseDouble(lon);
-			/*
-			if (Double.parseDouble(lat) == 33.98 && Double.parseDouble(lon) == -84.00  ) {
-				redDot.setX(50);
-				redDot.setY(50);
-			}
-			*/
-			Toast.makeText(context, "GPS lati " +gpsLocation[0]+" long "+ gpsLocation[1] , Toast.LENGTH_LONG).show();
-			// this part of the code is where the red dot is getting moved. 
-			double pixPerSecLon = 44.6973;
-			double pixPerSecLat = 44.6961;
-			double pixPerMeterLon = (widthAtCurrentScale) /1000; // p/m
-			double pixPerMeterLat = (heightAtCurrentScale)/1700;// p/m
-			double xOfCBuildingPix = (imageViewBackGround.getX())+(widthAtCurrentScale*0.54342); // percent meters across X back of cBuliding
-			double yOfCBuildingPix = (imageViewBackGround.getY())+(heightAtCurrentScale*0.36336);// percent meters across y back of cBuilding 
-			double cBuildingLat = 33.980; // lat = x  // check sigfigs
-			double cBuildingLon = -84.006;// lon = y
-			double GPSLatDif = gpsLocation[0] - cBuildingLat;
-			double GPSLonDif = gpsLocation[1] - cBuildingLon;
-			double locationX = xOfCBuildingPix+(((-1*(gpsLocation[0] - cBuildingLat))*30.86)* pixPerMeterLat);// answer in Pix 
-			double locationY = yOfCBuildingPix+(((-1*(gpsLocation[1] - cBuildingLon))*30.92)* pixPerMeterLon);// answer in Pix 
-									//cBuilding				 GPS                       M		 Pix
-			xPixVal = locationX;
-			yPixVal = locationY;
-			Log.d("Test", "locationX="+ locationX + " locationY="+locationY +" xPixVal="+xPixVal +" yPixVal="+yPixVal+" gpsLatDif="+GPSLatDif+" gpsLonDif="+GPSLonDif);
-				
-			redDot.setX((float) xPixVal);
-			redDot.setY((float) yPixVal);
-			//redDot.setX((float) (xPixVal+(((childView.getWidth()*matrixDataOfBackground[0])-childView.getWidth())/2)));
-			//redDot.setY((float) (yPixVal+(((childView.getWidth()*matrixDataOfBackground[0])-childView.getWidth())/2)));	
-			//viewArray[0] = (float) ((xPixVal-imageViewBackGround.getWidth()/64)/imageViewBackGround.getWidth()); // Scale conversion may taken out after testing 
-			//viewArray[1] = (float) ((yPixVal-imageViewBackGround.getWidth()/64)/imageViewBackGround.getHeight()); // widthAtCurrentScale/64 = half of redDot size
-
-			imageViewBackGround.invalidate();
-			redDot.invalidate();
-			//Log.d("GPS Data", "GPS latitude=" +latitude+ " longitude="+longitude+" latSubstring="+lat+" lonSubstring="+lon+ " X = "+imageViewBackGround.getWidth()+" Y = "+ imageViewBackGround.getHeight());
+			Toast.makeText(context, "GPS lati " +latitude+" long "+ longitude , Toast.LENGTH_LONG).show();
 		}
-		
 		// So 1 second of latitude = 30.86 meters, or in feet = 101.2 ft.
 		// 1 second of longitude = 30.922 meters.
 		// C Building lati 33.98067802886346 long -84.0065738567545
-
-		private int height() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
 
 		@Override
 		public void onProviderDisabled(String provider) {
@@ -298,125 +111,4 @@ public class MapActivity extends Activity {
 			
 		}	
 	}
-	
-	public class TouchListener implements OnTouchListener {
-
-		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-			ImageView view = (ImageView) v;
-
-			if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
-				oldMatrix.set(matrix);
-				start.set(event.getX(), event.getY());
-				mode = State.DRAGGING;
-			}
-
-			if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_DOWN) {
-				oldDistance = spaceBetweenTwoFingers(event);
-				oldMatrix.set(matrix);
-				midPointBetweenTwoFingers(middle, event);
-				mode = State.ZOOMING;
-			}
-
-			if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_POINTER_UP) {
-				mode = State.NONE;
-			}
-
-			if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_MOVE) {
-				if (mode == State.DRAGGING) {
-					matrix.set(oldMatrix);
-					matrix.postTranslate(event.getX() - start.x, event.getY()
-							- start.y);
-				} else if (mode == State.ZOOMING) {
-					float newDistance = spaceBetweenTwoFingers(event);
-					matrix.set(oldMatrix);
-					float scale = newDistance / oldDistance;
-					matrix.postScale(scale, scale, middle.x, middle.y);
-				}
-			}
-			
-			view.setImageMatrix(matrix);
-			String s = matrix.toShortString();
-			String[] strMatrix = s.split("[\\[ \\] , ]"); 
-			backGroundImageScaleAndVal(matrixDataOfBackground,strMatrix);
-			if(firstRun == true){
-				redDotArray = firstRunDataLog(redDot);
-				imageButtonABuildingArray = firstRunDataLog(imageButtonABuilding);
-				imageButtonBBuildingArray = firstRunDataLog(imageButtonBBuilding);
-				imageButtonCBuildingArray = firstRunDataLog(imageButtonCBuilding);
-				imageButtonDBuildingArray = firstRunDataLog(imageButtonDBuilding);
-				imageButtonFBuildingArray = firstRunDataLog(imageButtonFBuilding);
-				imageButtonLBuildingArray = firstRunDataLog(imageButtonLBuilding);
-				imageButtonStudentCenterArray = firstRunDataLog(imageButtonStudentCenter);
-				
-				oldXVal = matrixDataOfBackground[1];// current x val
-				oldYVal = matrixDataOfBackground[3];// current y val
-				firstRun = false;
-			}
-			viewsAutoScaleAndGroup( redDot, redDotArray);
-			viewsAutoScaleAndGroup( imageButtonABuilding, imageButtonABuildingArray);
-			viewsAutoScaleAndGroup( imageButtonBBuilding, imageButtonBBuildingArray);
-			viewsAutoScaleAndGroup( imageButtonCBuilding, imageButtonCBuildingArray);
-			viewsAutoScaleAndGroup( imageButtonDBuilding, imageButtonDBuildingArray);
-			viewsAutoScaleAndGroup( imageButtonFBuilding, imageButtonFBuildingArray);
-			viewsAutoScaleAndGroup( imageButtonLBuilding, imageButtonLBuildingArray);
-			viewsAutoScaleAndGroup( imageButtonStudentCenter, imageButtonStudentCenterArray);
-
-			/*
-			Log.d("H/W view", view.getWidth()+"=W - H="+view.getHeight());
-			Log.d("Height/Width", heightAtCurrentScale +"=W - H="+widthAtCurrentScale);
-			Log.d("xScale/yScal", backGroundImageScaleAndValArray[1]+"=W - H="+backGroundImageScaleAndValArray[3]);
-			*/
-			oldXScale = matrixDataOfBackground[0];
-			oldYScale = matrixDataOfBackground[2];
-			oldXVal = matrixDataOfBackground[1];
-			oldYVal = matrixDataOfBackground[3];
-			return true;
-		}
-		private void viewsAutoScaleAndGroup( View childView, float[] viewArray) {
-			double changeInX = matrixDataOfBackground[1] -oldXVal; // baseImage + baseImage Martix[] = scale groups // This is a note for latter about to scale groups of images. 
-			double changeInY = matrixDataOfBackground[3] -oldYVal;
-			widthAtCurrentScale = imageViewBackGround.getWidth()*matrixDataOfBackground[0];
-			heightAtCurrentScale = imageViewBackGround.getHeight()*matrixDataOfBackground[2];	
-			
-			if(oldXScale != matrixDataOfBackground[0] || oldYScale != matrixDataOfBackground[2]){//0 = viewXOffSet //1 = viewYOffSet // 2 = oldX // 3 = oldY 	
-				childView.setX((float) (matrixDataOfBackground[1]+(viewArray[0]*widthAtCurrentScale)) ); // + . . . . )
-				childView.setY((float) (matrixDataOfBackground[3]+(viewArray[1]*heightAtCurrentScale)) );
-				childView.setX((float) (childView.getX()+(((childView.getWidth()*matrixDataOfBackground[0])-childView.getWidth())/2)));
-				childView.setY((float) (childView.getY()+(((childView.getWidth()*matrixDataOfBackground[2])-childView.getHeight())/2)));
-				childView.setScaleX((float) matrixDataOfBackground[0]);
-				childView.setScaleY((float) matrixDataOfBackground[2]);	
-			}else{
-				childView.setX((float) (childView.getX()+(changeInX)));
-				childView.setY((float) (childView.getY()+(changeInY)));
-			}	
-			viewArray[2] = childView.getX();
-			viewArray[3] = childView.getY();
-		}
-		/** backGroundImageScaleAndVal
-		 *  0 = xScale
-		 *  1 = xVale
-		 *  2 = yScale
-		 *  3 = yVale
-		 */
-		private void backGroundImageScaleAndVal(double[] backGroundImageScaleAndValArray, String[] strMatrix) {
-			backGroundImageScaleAndValArray[0] = Double.parseDouble(strMatrix[1]); // 0 = xScale
-			backGroundImageScaleAndValArray[1] = Double.parseDouble(strMatrix[5]); // 1 = xVale
-			backGroundImageScaleAndValArray[2] = Double.parseDouble(strMatrix[9]); // 2 = yScale
-			backGroundImageScaleAndValArray[3] = Double.parseDouble(strMatrix[11]);// 3 = yVale
-		}
-
-		public float[] firstRunDataLog(View view){
-			float[] viewArray = new float[6];
-			viewArray[0] = view.getX()/imageViewBackGround.getWidth(); //0 = viewXOffSet 
-			viewArray[1] = view.getY()/imageViewBackGround.getHeight(); //1 = viewYOffSet // why is the width of this background image not adjusted for scale?1
-			viewArray[2] = view.getX();// 2 = X 	
-			viewArray[3] = view.getY();// 3 = Y 
-			viewArray[4] = view.getX();// 4 = oldX 
-			viewArray[5] = view.getY();// 5 = oldY
-			
-			return viewArray;
-		}
-	
-}
 }
--- src/edu/ggc/it/map/MapView.java
+++ src/edu/ggc/it/map/MapView.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
@@ -15,13 +16,19 @@ public class MapView extends View {
 	Activity activity;
 	
 	private ScaleGestureDetector scaleGestureDetector;
+	private GestureDetector gestureDetector;
+	private float newX, newY;
 	private float mScaleFactor = 1.f;
 
 	public MapView(Context context){
 		super(context);
 		setDrawingCacheEnabled(true);	
-		setBackgroundColor(Color.BLACK);
+		setBackgroundColor(Color.YELLOW);
 	    scaleGestureDetector = new ScaleGestureDetector(context, new ScaleListener());
+	    // GestureDetector.SimpleOnGestureListener
+	    gestureDetector = new GestureDetector(context,  new GestureListener());
+	    newX = 0;
+	    newY = 0;
 	}
 
 	@Override
@@ -37,9 +44,29 @@ public class MapView extends View {
 
 	    canvas.save();
 	    canvas.scale(mScaleFactor, mScaleFactor);
+	    canvas.translate(newX, newY);
 		Bitmap pic = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_building_b_map);
 		canvas.drawBitmap(pic, canvas.getWidth()/2, canvas.getHeight()/2, null); 
 	    canvas.restore();
+	}
+	
+	private class GestureListener extends GestureDetector.SimpleOnGestureListener{
+		@Override
+		public boolean onDown(MotionEvent e) {// worng
+			
+			// google how to use gestureDetector onDown
+	        
+	        newY = e.getY();
+
+	        // Don't let the object get too small or too large.
+	        //mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
+
+	        invalidate();
+			
+			
+			
+			return super.onDown(e);	
+		}
 	}
 
 	private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
