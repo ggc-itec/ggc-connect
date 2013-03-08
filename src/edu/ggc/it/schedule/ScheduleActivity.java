0d56cc3f70607346415e75042e3f53af5cc6fb11
rramsaro <rramsaro@ggc.edu>
2013-03-08 18:50:29 -0500

    Implemented a Separated List View

--- src/edu/ggc/it/schedule/ScheduleActivity.java
+++ src/edu/ggc/it/schedule/ScheduleActivity.java
@@ -1,19 +1,26 @@
 package edu.ggc.it.schedule;
 
-import edu.ggc.it.R;
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import edu.ggc.it.R;
 
 public class ScheduleActivity extends Activity {
 
@@ -21,16 +28,46 @@ public class ScheduleActivity extends Activity {
 	private ScheduleDatabase database;
 	private SimpleCursorAdapter cursorAdapter;
 	private ListView list;
+	
+	public final static String ITEM_TITLE = "title";
+	public final static String ITEM_CAPTION = "caption";
+	
+	/**
+	 * This creates a list item in the separated list view for the activity
+	 * @param title The main text for the item
+	 * @param caption A short description of the ite,
+	 * @return A HashMap with the item title and caption
+	 */
+	public Map<String,?> createItem(String title, String caption) {
+		Map<String,String> item = new HashMap<String,String>();
+		item.put(ITEM_TITLE, title);
+		item.put(ITEM_CAPTION, caption);
+		return item;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_schedule);
 		scheduleContext = this;
 		
-		list = (ListView) findViewById(R.id.schedule_list_view);
-		list.setOnItemClickListener(new ScheduleActivityListListener());
-
+		// list stuff
+		List<Map<String,?>> security = new LinkedList<Map<String,?>>();
+		security.add(createItem("Remember passwords", "Save usernames and passwords for Web sites"));
+		security.add(createItem("Clear passwords", "Save usernames and passwords for Web sites"));
+		security.add(createItem("Show security warnings", "Show warning if there is a problem with a site's security"));
+		
+		// create our list and custom adapter
+		SeparatedListAdapter adapter = new SeparatedListAdapter(this);
+		adapter.addSection("Array test", new ArrayAdapter<String>(this,
+			R.layout.schedule_list_item, new String[] { "First item", "Item two" }));
+		adapter.addSection("Security", new SimpleAdapter(this, security, R.layout.activity_schedule, 
+			new String[] { ITEM_TITLE, ITEM_CAPTION }, new int[] { R.id.list_complex_title, R.id.list_complex_caption }));
+		
+		ListView list = new ListView(this);
+		list.setAdapter(adapter);
+		this.setContentView(list);
+		// end list stuff
+		
 		// open new database
 		database = new ScheduleDatabase(scheduleContext);
 		database.open();
@@ -47,7 +84,8 @@ public class ScheduleActivity extends Activity {
 	}
 
 	private void populateList() {
-		Cursor cursor = database.queryAll();
+		//TODO: Use the new list adapter method to populate the list
+		/*Cursor cursor = database.queryAll();
 		startManagingCursor(cursor);
 
 		String[] from = new String[] { ScheduleDatabase.KEY_NAME, ScheduleDatabase.KEY_BUILDING_LOCATION };
@@ -56,7 +94,7 @@ public class ScheduleActivity extends Activity {
 				cursor, from, to);
 
 		list.setAdapter(cursorAdapter);
-		registerForContextMenu(list.getRootView());
+		registerForContextMenu(list.getRootView());*/
 	}
 
 	private boolean classesExist() {
