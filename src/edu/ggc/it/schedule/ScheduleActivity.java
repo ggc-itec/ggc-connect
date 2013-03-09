b8c9940d012d8b76856b4c7102182fc31419aa61
rramsaro <rramsaro@ggc.edu>
2013-03-08 19:13:23 -0500

    Created dummy stuff to test separatedlistview

--- src/edu/ggc/it/schedule/ScheduleActivity.java
+++ src/edu/ggc/it/schedule/ScheduleActivity.java
@@ -28,18 +28,21 @@ public class ScheduleActivity extends Activity {
 	private ScheduleDatabase database;
 	private SimpleCursorAdapter cursorAdapter;
 	private ListView list;
-	
+
 	public final static String ITEM_TITLE = "title";
 	public final static String ITEM_CAPTION = "caption";
-	
+
 	/**
 	 * This creates a list item in the separated list view for the activity
-	 * @param title The main text for the item
-	 * @param caption A short description of the ite,
+	 * 
+	 * @param title
+	 *            The main text for the item
+	 * @param caption
+	 *            A short description of the ite,
 	 * @return A HashMap with the item title and caption
 	 */
-	public Map<String,?> createItem(String title, String caption) {
-		Map<String,String> item = new HashMap<String,String>();
+	public Map<String, ?> createItem(String title, String caption) {
+		Map<String, String> item = new HashMap<String, String>();
 		item.put(ITEM_TITLE, title);
 		item.put(ITEM_CAPTION, caption);
 		return item;
@@ -49,25 +52,7 @@ public class ScheduleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		scheduleContext = this;
-		
-		// list stuff
-		List<Map<String,?>> security = new LinkedList<Map<String,?>>();
-		security.add(createItem("Remember passwords", "Save usernames and passwords for Web sites"));
-		security.add(createItem("Clear passwords", "Save usernames and passwords for Web sites"));
-		security.add(createItem("Show security warnings", "Show warning if there is a problem with a site's security"));
-		
-		// create our list and custom adapter
-		SeparatedListAdapter adapter = new SeparatedListAdapter(this);
-		adapter.addSection("Array test", new ArrayAdapter<String>(this,
-			R.layout.schedule_list_item, new String[] { "First item", "Item two" }));
-		adapter.addSection("Security", new SimpleAdapter(this, security, R.layout.activity_schedule, 
-			new String[] { ITEM_TITLE, ITEM_CAPTION }, new int[] { R.id.list_complex_title, R.id.list_complex_caption }));
-		
-		ListView list = new ListView(this);
-		list.setAdapter(adapter);
-		this.setContentView(list);
-		// end list stuff
-		
+
 		// open new database
 		database = new ScheduleDatabase(scheduleContext);
 		database.open();
@@ -76,25 +61,61 @@ public class ScheduleActivity extends Activity {
 			CharSequence text = "No courses found on your schedule. Use the menu to add a course.";
 			int duration = Toast.LENGTH_LONG;
 			Toast toast = Toast.makeText(scheduleContext, text, duration);
-			toast.show();;
-		} else {
+			toast.show();
+			// call populate list for testing purposes for now
 			populateList();
+		} else {
+			//populateList();
 		}
 
 	}
 
 	private void populateList() {
-		//TODO: Use the new list adapter method to populate the list
-		/*Cursor cursor = database.queryAll();
-		startManagingCursor(cursor);
+		// A LinkedList for each day that holds the classes that are on that day
+		List<Map<String, ?>> monday = new LinkedList<Map<String, ?>>();
+		List<Map<String, ?>> tuesday = new LinkedList<Map<String, ?>>();
+		List<Map<String, ?>> wednesday = new LinkedList<Map<String, ?>>();
+		List<Map<String, ?>> thursday = new LinkedList<Map<String, ?>>();
+		List<Map<String, ?>> friday = new LinkedList<Map<String, ?>>();
+		List<Map<String, ?>> saturday = new LinkedList<Map<String, ?>>();
+		
+		//TODO: Database needs to pull info here to populate lists
+		monday.add(createItem("Class Name", "Start/End time, location etc"));
 
-		String[] from = new String[] { ScheduleDatabase.KEY_NAME, ScheduleDatabase.KEY_BUILDING_LOCATION };
-		int[] to = new int[] { R.id.class_name, R.id.building_location };
-		cursorAdapter = new SimpleCursorAdapter(this, R.layout.schedule_list_row,
-				cursor, from, to);
+		// Create header sections and add populated lists
+		SeparatedListAdapter adapter = new SeparatedListAdapter(this);
+		createListSection(adapter, "Monday", monday);
+		createListSection(adapter, "Tuesday", tuesday);
+		createListSection(adapter, "Wednesday", wednesday);
+		createListSection(adapter, "Thursday", thursday);
+		createListSection(adapter, "Friday", friday);
+		createListSection(adapter, "Saturday", saturday);
+		
+		// Display the list
+		ListView list = new ListView(this);
+		list.setAdapter(adapter);
+		this.setContentView(list);
 
-		list.setAdapter(cursorAdapter);
-		registerForContextMenu(list.getRootView());*/
+		/*
+		 * Cursor cursor = database.queryAll(); startManagingCursor(cursor);
+		 * 
+		 * String[] from = new String[] { ScheduleDatabase.KEY_NAME,
+		 * ScheduleDatabase.KEY_BUILDING_LOCATION }; int[] to = new int[] {
+		 * R.id.class_name, R.id.building_location }; cursorAdapter = new
+		 * SimpleCursorAdapter(this, R.layout.schedule_list_row, cursor, from,
+		 * to);
+		 * 
+		 * list.setAdapter(cursorAdapter);
+		 * registerForContextMenu(list.getRootView());
+		 */
+	}
+
+	private void createListSection(SeparatedListAdapter adapter,
+			String sectionHeader, List<Map<String, ?>> list) {
+		adapter.addSection(sectionHeader, new SimpleAdapter(this, list,
+				R.layout.activity_schedule, new String[] { ITEM_TITLE,
+						ITEM_CAPTION }, new int[] { R.id.list_complex_title,
+						R.id.list_complex_caption }));
 	}
 
 	private boolean classesExist() {
@@ -124,24 +145,26 @@ public class ScheduleActivity extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
 	public void updateDatabase(long rowId, boolean create) {
-		Intent intent = new Intent(scheduleContext, ScheduleUpdateActivity.class);
+		Intent intent = new Intent(scheduleContext,
+				ScheduleUpdateActivity.class);
 		if (!create) {
 			intent.putExtra("rowID", rowId);
 		}
 		startActivity(intent);
 	}
-	
+
 	public class ScheduleActivityListListener implements OnItemClickListener {
 
 		@Override
-		public void onItemClick(AdapterView<?> list, View view, int position, long rowId) {
+		public void onItemClick(AdapterView<?> list, View view, int position,
+				long rowId) {
 			updateDatabase(rowId, false);
 		}
-		
+
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
