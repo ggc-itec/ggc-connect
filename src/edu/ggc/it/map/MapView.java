93c1c6bc35424472069af26f127d37c8a747c508
JohnnyLynch <alynch@ggc.edu>
2013-03-16 14:58:19 -0400

    Just got custom buttons to work with zooming and scrolling.

--- src/edu/ggc/it/map/MapView.java
+++ src/edu/ggc/it/map/MapView.java
@@ -1,27 +1,18 @@
 package edu.ggc.it.map;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.PointF;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 import edu.ggc.it.R;
 
@@ -30,37 +21,61 @@ public class MapView extends View {
 	Context context;
 	private ScaleGestureDetector scaleGestureDetector;
 	GestureDetector gestureDetector;
-	private float newX, newY;
-	private float mScaleFactor = 1.f;
+	HashMap<String, Float> scaleReferenceHashMap;
 	private Bitmap pic;
-	public enum State {
-		NONE, DRAGGING, ZOOMING
-	};
+	private Bitmap aBuilding;
+	private ArrayList<Float> canvasX;
+	private ArrayList<Float> canvasY;
+	private float newX, newY;
+	private float mScaleFactor = 0.25f; //1.f
 
-	float oldDistance = 1f;
-	double oldXVal;
-	double oldYVal;
-	double[] matrixDataOfBackground;
-	double[] gpsLocation;
-	float[] redDotArray;
-	double yPixVal;
-	double xPixVal;
-	double heightAtCurrentScale;
-	double widthAtCurrentScale;
-	double oldXScale;
-	double oldYScale;
-	Boolean firstRun;
 
 	public MapView(Context context) {
 		super(context);
 		this.context = context;
 		setDrawingCacheEnabled(true);
 		setBackgroundColor(Color.YELLOW);
-		pic = BitmapFactory.decodeResource(getResources(),);
+		pic = BitmapFactory.decodeResource(getResources(),R.drawable.ggc_map);
+		aBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+		makeScaleReferenceHashMap();
 		scaleGestureDetector = new ScaleGestureDetector(context,new ScaleListener());
 		gestureDetector = new GestureDetector(context, new GestureListener());
 		newX = 0;
 		newY = 0;
+		canvasX = new ArrayList<Float>();
+		canvasY = new ArrayList<Float>();
+		setCanvasXY(newX, newY);
+	}
+	/*
+	 * The setCanvasXY and getCanvasX/ getCanvasY where made to help keep track of where
+	 * start of background image is.
+	 * 
+	 */
+	private void setCanvasXY(float x, float y){
+		canvasX.add(x);
+		canvasY.add(y);	
+	}
+	
+	private float getCanvasX(){
+		float xSum = 0;
+		for(float f: canvasX){
+			xSum+=f;
+		}
+		return xSum;
+	}
+	
+	private float getCanvasY(){
+		float ySum = 0;
+		for(float f: canvasY){
+			ySum+=f;
+		}
+		return ySum;
+	}
+	
+	private void makeScaleReferenceHashMap(){
+		scaleReferenceHashMap = new HashMap<String,Float>();
+		scaleReferenceHashMap.put("A_BUILDING_X",(float)(((pic.getWidth()/2)-aBuilding.getWidth()/2)-(pic.getWidth()/14)));
+		scaleReferenceHashMap.put("A_BUILDING_Y",(float)(((pic.getHeight()/2)-aBuilding.getHeight()/2)-(pic.getHeight()/14)) );
 	}
 
 	@Override
@@ -68,12 +83,35 @@ public class MapView extends View {
 		// Both gestureDerector and ScaleGestureDetector are always getting MotionEven
 		// this is causing scaling and scrolling to happen at the same time. 
 		// TODO find away to fix this problem.
+		
+		if(ev.getAction() == MotionEvent.ACTION_DOWN){
+		   float touchX = ev.getX();
+		   float touchY = ev.getY();
+		   
+		   float xOffSet = getCanvasX();
+		   float yOffSet = getCanvasY();
+		   
+		   float aBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("A_BUILDING_X")*mScaleFactor);
+		   float aBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("A_BUILDING_X")+aBuilding.getWidth())*mScaleFactor);
+				   
+		   float aBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("A_BUILDING_Y")*mScaleFactor);
+		   float aBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("A_BUILDING_Y")+aBuilding.getWidth())*mScaleFactor);
+				   
 
-		//if (ev.getAction() == MotionEvent.ACTION_SCROLL) {
-			gestureDetector.onTouchEvent(ev);
-		//} else {
+		   if(touchX >= aBuildingXMin && touchX <= aBuildingXMax){   
+			   if(touchY >= aBuildingYMin && touchY <= aBuildingYMax){
+				   Toast.makeText(context, touchX+" "+touchY+"\n"+ aBuildingXMin+"~~"+aBuildingYMin, 1000).show();
+			   }
+		   }
+		   
+
+		}
+
+		if (ev.getPointerCount() == 1) {
+		    gestureDetector.onTouchEvent(ev);
+		} else {
 			scaleGestureDetector.onTouchEvent(ev);
-		//}
+		}
 		return true;
 	}
 
@@ -83,70 +121,24 @@ public class MapView extends View {
 		canvas.save();
 		canvas.scale(mScaleFactor, mScaleFactor);
 		canvas.translate(newX, newY);
-		canvas.drawBitmap(pic, canvas.getWidth() / 2, canvas.getHeight() / 2,null); // Need some kind of constant
+		canvas.drawBitmap(pic, 0, 0,null); 
+		// need to set up scaling data bank
+		canvas.drawBitmap(aBuilding,scaleReferenceHashMap.get("A_BUILDING_X"),scaleReferenceHashMap.get("A_BUILDING_Y"),null);
 		canvas.restore();
 	}
+	
 
 	private class GestureListener extends GestureDetector.SimpleOnGestureListener {
 		
-		/*
-		@Override
-		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,float velocityY) {
-		
-			Toast.makeText(context, "onFling", 750).show();
-			newX = newX+velocityX;
-			newY = newY+velocityY;
-			invalidate();
-			return super.onFling(e1, e2, velocityX, velocityY);
-		}
-		*/
 		@Override
 		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-			
-			//Toast.makeText(context, "onScroll", 750).show();
 			newX = newX-distanceX;
 			newY = newY-distanceY;
+			setCanvasXY(-distanceX, -distanceY);// scrolling and zooming are throwing off canvasXY data
 			invalidate();
 			return super.onScroll(e1, e2, distanceX, distanceY);
-			
 		};
 
-		@Override
-		public boolean onDown(MotionEvent e) {// worng
-			
-			//Toast.makeText(context, "onDown", 750).show();
-
-			/*
-			final int historySize = e.getHistorySize();
-			final int pointerCount = e.getPointerCount();
-			float firstX = 0, firstY = 0, newestX = 0, newestY = 0; 
-			for (int h = 0; h < historySize; h++) {
-				if (h == 0 || h == historySize - 1) {
-					for (int p = 0; p < pointerCount; p++) {
-					
-						if(h == 0){
-								firstX = e.getHistoricalX(p,h);
-								firstY = e.getHistoricalY(p, h);
-						}else if ( h == historySize-1 ){
-								newestX = e.getHistoricalX(p,h);
-								newestY = e.getHistoricalY(p,h);	
-						}
-						
-					}
-				}
-				
-			}
-			
-			newX = newX +(newestX - firstX);
-			newY = newY +(newestY - firstY);
-			
-			Toast.makeText(context, "onDown", 500).show();
-			Log.d("Test", "onDown");
-
-			invalidate();
-			*/
-			return super.onDown(e);
-		}
 	}
 
 	private class ScaleListener extends
@@ -154,29 +146,11 @@ public class MapView extends View {
 		@Override
 		public boolean onScale(ScaleGestureDetector detector) {
 			mScaleFactor *= detector.getScaleFactor();
-
 			// Don't let the object get too small or too large.
 			mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
-
 			invalidate();
 			return true;
 		}
-	}
-
-	/**
-	 * MapActivity has an image of GGC that helps users know where they are.
-	 */
-
-	private float spaceBetweenTwoFingers(MotionEvent event) {
-		float x = event.getX(0) - event.getX(1);
-		float y = event.getY(0) - event.getY(1);
-		return (float) Math.sqrt(x * x + y * y);
-	}
-
-	private void midPointBetweenTwoFingers(PointF point, MotionEvent event) {
-		float x = event.getX(0) + event.getX(1);
-		float y = event.getY(0) + event.getY(1);
-		point.set(x / 2, y / 2);
 	}
 
 }
