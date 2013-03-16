d343184324cc7d5eee6816dc0be41a9fe5deb2ad
JohnnyLynch <alynch@ggc.edu>
2013-03-16 19:22:20 -0400

    Now all of the buttons are in place for all the of the buildings. Next commit will have the activits for the buildings popup when they are touched.

--- src/edu/ggc/it/map/MapView.java
+++ src/edu/ggc/it/map/MapView.java
@@ -2,7 +2,6 @@ package edu.ggc.it.map;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -24,20 +23,36 @@ public class MapView extends View {
 	HashMap<String, Float> scaleReferenceHashMap;
 	private Bitmap pic;
 	private Bitmap aBuilding;
+	private Bitmap bBuilding;
+	private Bitmap cBuilding;
+	private Bitmap dBuilding;
+	private Bitmap fBuilding;
+	private Bitmap lBuilding;
+	private Bitmap sBuilding;
+	
 	private ArrayList<Float> canvasX;
 	private ArrayList<Float> canvasY;
 	private float newX, newY;
 	private float mScaleFactor = 0.25f; //1.f
+	private float scaledHeight;
+	private float scaledWidth;
+	private boolean firstRun = true;
+
 
 
 	public MapView(Context context) {
 		super(context);
 		this.context = context;
 		setDrawingCacheEnabled(true);
-		setBackgroundColor(Color.YELLOW);
 		pic = BitmapFactory.decodeResource(getResources(),R.drawable.ggc_map);
 		aBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
-		makeScaleReferenceHashMap();
+		bBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+		cBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+		dBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+		fBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+		lBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+		sBuilding = BitmapFactory.decodeResource(getResources(), R.drawable.ggc_scale_gray_button);
+
 		scaleGestureDetector = new ScaleGestureDetector(context,new ScaleListener());
 		gestureDetector = new GestureDetector(context, new GestureListener());
 		newX = 0;
@@ -74,8 +89,27 @@ public class MapView extends View {
 	
 	private void makeScaleReferenceHashMap(){
 		scaleReferenceHashMap = new HashMap<String,Float>();
-		scaleReferenceHashMap.put("A_BUILDING_X",(float)(((pic.getWidth()/2)-aBuilding.getWidth()/2)-(pic.getWidth()/14)));
-		scaleReferenceHashMap.put("A_BUILDING_Y",(float)(((pic.getHeight()/2)-aBuilding.getHeight()/2)-(pic.getHeight()/14)) );
+		
+		scaleReferenceHashMap.put("A_BUILDING_X",(float) ( (scaledWidth*(952/1400.0)) -(aBuilding.getWidth()/2)));
+		scaleReferenceHashMap.put("A_BUILDING_Y",(float) ( (scaledHeight*(568/1120.0)) -(aBuilding.getHeight()/2)));
+		
+		scaleReferenceHashMap.put("B_BUILDING_X",(float)(((pic.getWidth()/2)-bBuilding.getWidth()/2)-(pic.getWidth()/14)));
+		scaleReferenceHashMap.put("B_BUILDING_Y",(float)(((pic.getHeight()/2)-bBuilding.getHeight()/2)-(pic.getHeight()/14)));
+		
+		scaleReferenceHashMap.put("C_BUILDING_X",(float) ( (scaledWidth*(510/1400.0)) -(cBuilding.getWidth()/2)));
+		scaleReferenceHashMap.put("C_BUILDING_Y",(float) ( (scaledHeight*(550/1120.0)) -(cBuilding.getHeight()/2)));
+		
+		scaleReferenceHashMap.put("D_BUILDING_X",(float) ( (scaledWidth*(1165/1400.0)) -(dBuilding.getWidth()/2)));
+		scaleReferenceHashMap.put("D_BUILDING_Y",(float) ( (scaledHeight*(611/1120.0)) -(dBuilding.getHeight()/2)));
+		
+		scaleReferenceHashMap.put("F_BUILDING_X",(float) ( (scaledWidth*(1094/1400.0)) -(fBuilding.getWidth()/2)));
+		scaleReferenceHashMap.put("F_BUILDING_Y",(float) ( (scaledHeight*(536/1120.0)) -(fBuilding.getHeight()/2)));
+		
+		scaleReferenceHashMap.put("L_BUILDING_X",(float) ( (scaledWidth*(670/1400.0)) -(lBuilding.getWidth()/2)));
+		scaleReferenceHashMap.put("L_BUILDING_Y",(float) ( (scaledHeight*(623/1120.0)) -(lBuilding.getHeight()/2)));
+		
+		scaleReferenceHashMap.put("S_BUILDING_X",(float) ( (scaledWidth*(567/1400.0)) -(sBuilding.getWidth()/2)));
+		scaleReferenceHashMap.put("S_BUILDING_Y",(float) ( (scaledHeight*(640/1120.0)) -(sBuilding.getHeight()/2)));
 	}
 
 	@Override
@@ -86,25 +120,10 @@ public class MapView extends View {
 		
 		if(ev.getAction() == MotionEvent.ACTION_DOWN){
 		   float touchX = ev.getX();
-		   float touchY = ev.getY();
-		   
+		   float touchY = ev.getY();  
 		   float xOffSet = getCanvasX();
 		   float yOffSet = getCanvasY();
-		   
-		   float aBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("A_BUILDING_X")*mScaleFactor);
-		   float aBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("A_BUILDING_X")+aBuilding.getWidth())*mScaleFactor);
-				   
-		   float aBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("A_BUILDING_Y")*mScaleFactor);
-		   float aBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("A_BUILDING_Y")+aBuilding.getWidth())*mScaleFactor);
-				   
-
-		   if(touchX >= aBuildingXMin && touchX <= aBuildingXMax){   
-			   if(touchY >= aBuildingYMin && touchY <= aBuildingYMax){
-				   Toast.makeText(context, touchX+" "+touchY+"\n"+ aBuildingXMin+"~~"+aBuildingYMin, 1000).show();
-			   }
-		   }
-		   
-
+		   methodBasedButtonActionListener(xOffSet,yOffSet, touchX, touchY);
 		}
 
 		if (ev.getPointerCount() == 1) {
@@ -115,15 +134,128 @@ public class MapView extends View {
 		return true;
 	}
 
+	private void methodBasedButtonActionListener(float xOffSet, float yOffSet, float touchX, float touchY) {
+		   ///A
+		   float aBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("A_BUILDING_X")*mScaleFactor);
+		   float aBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("A_BUILDING_X")+aBuilding.getWidth())*mScaleFactor);
+				   
+		   float aBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("A_BUILDING_Y")*mScaleFactor);
+		   float aBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("A_BUILDING_Y")+aBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= aBuildingXMin && touchX <= aBuildingXMax){   
+			   if(touchY >= aBuildingYMin && touchY <= aBuildingYMax){
+				   Toast.makeText(context,"aBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }
+		   ///B
+		   float bBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("B_BUILDING_X")*mScaleFactor);
+		   float bBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("B_BUILDING_X")+bBuilding.getWidth())*mScaleFactor);
+				   
+		   float bBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("B_BUILDING_Y")*mScaleFactor);
+		   float bBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("B_BUILDING_Y")+bBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= bBuildingXMin && touchX <= bBuildingXMax){   
+			   if(touchY >= bBuildingYMin && touchY <= bBuildingYMax){
+				   Toast.makeText(context,"bBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }	
+		   ///C
+		   float cBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("C_BUILDING_X")*mScaleFactor);
+		   float cBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("C_BUILDING_X")+cBuilding.getWidth())*mScaleFactor);
+				   
+		   float cBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("C_BUILDING_Y")*mScaleFactor);
+		   float cBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("C_BUILDING_Y")+cBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= cBuildingXMin && touchX <= cBuildingXMax){   
+			   if(touchY >= cBuildingYMin && touchY <= cBuildingYMax){
+				   Toast.makeText(context,"cBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }
+		   ///D
+		   float dBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("D_BUILDING_X")*mScaleFactor);
+		   float dBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("D_BUILDING_X")+dBuilding.getWidth())*mScaleFactor);
+				   
+		   float dBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("D_BUILDING_Y")*mScaleFactor);
+		   float dBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("D_BUILDING_Y")+dBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= dBuildingXMin && touchX <= dBuildingXMax){   
+			   if(touchY >= dBuildingYMin && touchY <= dBuildingYMax){
+				   Toast.makeText(context,"dBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }
+		   ///F
+		   float fBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("F_BUILDING_X")*mScaleFactor);
+		   float fBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("F_BUILDING_X")+fBuilding.getWidth())*mScaleFactor);
+				   
+		   float fBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("F_BUILDING_Y")*mScaleFactor);
+		   float fBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("F_BUILDING_Y")+fBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= fBuildingXMin && touchX <= fBuildingXMax){   
+			   if(touchY >= fBuildingYMin && touchY <= fBuildingYMax){
+				   Toast.makeText(context,"fBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }
+		   ///L
+		   float lBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("L_BUILDING_X")*mScaleFactor);
+		   float lBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("L_BUILDING_X")+lBuilding.getWidth())*mScaleFactor);
+				   
+		   float lBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("L_BUILDING_Y")*mScaleFactor);
+		   float lBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("L_BUILDING_Y")+lBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= lBuildingXMin && touchX <= lBuildingXMax){   
+			   if(touchY >= lBuildingYMin && touchY <= lBuildingYMax){
+				   Toast.makeText(context,"lBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }
+		   ///S
+		   float sBuildingXMin = (xOffSet*mScaleFactor)+(scaleReferenceHashMap.get("S_BUILDING_X")*mScaleFactor);
+		   float sBuildingXMax = (xOffSet*mScaleFactor)+((scaleReferenceHashMap.get("S_BUILDING_X")+sBuilding.getWidth())*mScaleFactor);
+				   
+		   float sBuildingYMin = (yOffSet*mScaleFactor)+(scaleReferenceHashMap.get("S_BUILDING_Y")*mScaleFactor);
+		   float sBuildingYMax = (yOffSet*mScaleFactor)+((scaleReferenceHashMap.get("S_BUILDING_Y")+sBuilding.getWidth())*mScaleFactor);
+				   
+		   if(touchX >= sBuildingXMin && touchX <= sBuildingXMax){   
+			   if(touchY >= sBuildingYMin && touchY <= sBuildingYMax){
+				   Toast.makeText(context,"sBuilding", 750).show();
+				   //Toast.makeText(context, touchX+" "+touchY+"\n"+ bBuildingXMin+"~~"+bBuildingYMin, 1000).show();
+			   }
+		   }
+		   
+	}
 	@Override
 	public void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
 		canvas.save();
 		canvas.scale(mScaleFactor, mScaleFactor);
 		canvas.translate(newX, newY);
-		canvas.drawBitmap(pic, 0, 0,null); 
-		// need to set up scaling data bank
+		canvas.drawBitmap(pic, 0, 0,null); 	
+		
+		
+		scaledWidth = pic.getScaledWidth(canvas);
+		scaledHeight = pic.getScaledHeight(canvas);
+		
+		/*
+		scaledWidth = canvas.getWidth();
+		scaledHeight = canvas.getHeight();
+		*/
+		
+		if(firstRun == true){
+			makeScaleReferenceHashMap();
+			firstRun = false;
+		}
 		canvas.drawBitmap(aBuilding,scaleReferenceHashMap.get("A_BUILDING_X"),scaleReferenceHashMap.get("A_BUILDING_Y"),null);
+		canvas.drawBitmap(bBuilding,scaleReferenceHashMap.get("B_BUILDING_X"),scaleReferenceHashMap.get("B_BUILDING_Y"),null);
+		canvas.drawBitmap(bBuilding,scaleReferenceHashMap.get("C_BUILDING_X"),scaleReferenceHashMap.get("C_BUILDING_Y"),null);
+		canvas.drawBitmap(bBuilding,scaleReferenceHashMap.get("D_BUILDING_X"),scaleReferenceHashMap.get("D_BUILDING_Y"),null);
+		canvas.drawBitmap(bBuilding,scaleReferenceHashMap.get("F_BUILDING_X"),scaleReferenceHashMap.get("F_BUILDING_Y"),null);
+		canvas.drawBitmap(bBuilding,scaleReferenceHashMap.get("L_BUILDING_X"),scaleReferenceHashMap.get("L_BUILDING_Y"),null);
+		canvas.drawBitmap(bBuilding,scaleReferenceHashMap.get("S_BUILDING_X"),scaleReferenceHashMap.get("S_BUILDING_Y"),null);
 		canvas.restore();
 	}
 	
