87af90b3f2581999d770810f49632e54001d3543
Leo Hernandez <lhernand@ggc.edu>
2013-04-01 12:15:13 -0400

    modified gym activity

--- src/edu/ggc/it/gym/GroupsActivity.java
+++ src/edu/ggc/it/gym/GroupsActivity.java
@@ -39,6 +39,9 @@ public class GroupsActivity extends Activity {
 
 
 	@Override
+	/***************************************************************************
+	 * this method creates all of the buttons
+	 **************************************************************************/
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_groups);
@@ -86,59 +89,46 @@ public class GroupsActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_groups, menu);
 		return true;
 	}
+	/*************************************************************************
+	 * Button listener shows the descriptions of each of the buttons when a button is clicked
+	 *************************************************************************** */
 	public class ButtonListener implements OnClickListener {
 		public void onClick(View view) {
 
 			if (view.getId() == R.id.muscle1 || view.getId() == R.id.muscle2){
-				//Toast.makeText(myContext, "Tone and build muscle mass utilizing different resistance tools.", Toast.LENGTH_LONG)
-				//.show();
 				muscleDesc();				
 			}
 			else if (view.getId() == R.id.boot1 || view.getId() == R.id.boot2){
 				bootDesc();
-				//Toast.makeText(myContext, "Boot camp is a group exercise class that mixes traditional calisthenics and body weight exercises with interval training and strength training.", Toast.LENGTH_LONG).show();
 			}
 			else if (view.getId() == R.id.yogaletes){
-				//Toast.makeText(myContext, "Combines Yoga and Pilates.", Toast.LENGTH_LONG).show();
 				yogaletesDesc();
 			}
 			else if (view.getId() == R.id.yoga){
 				yogaDesc();
-				//Toast.makeText(myContext, "Appropriate for the beginner who wants to build a strong foundation of basic Yoga postures and breathing techniques, as well as for the practitioner who wants to refine and master the fundamentals. It is an invitation to stretch, relax, unwind and de-stress.", Toast.LENGTH_LONG).show();
 			}
 			else if (view.getId() == R.id.interval1 || view.getId() == R.id.interval2 || view.getId() == R.id.interval3){
 				intervalDesc();
-				//Toast.makeText(myContext, "A great cardio & strength training workout.", Toast.LENGTH_LONG).show();
 			}
 			else if (view.getId() == R.id.kickboxing) {
 				kickDesc();
-				//Toast.makeText(myContext, "Martial arts-inspired interval workout that combines kickboxing with athletic drills.", Toast.LENGTH_LONG).show();
 			}
 			else if (view.getId() == R.id.strength1) {
 				strengthDesc();
-				//Toast.makeText(myContext, "This interval training routine blends strength and cardio alternating circuits using body weight, a variety of equipment and partner work. The circuit training workout is loaded with functional exercises designed to give you the ultimate challenge.", Toast.LENGTH_LONG).show();
+
 			}
 
 		}
 
 	}
-
-	private void showAddScheduleItemDialog() {
-		new AlertDialog.Builder(this)
-		// .setTitle("No Classes Found")
-		.setMessage(R.string.schedule_no_classes_popup)
-
-
-
-		.show();
-	}
+	
+	/*************************************************************************
+	 * Shows the description of the MUSCLE STRENGTHING class
+	 *************************************************************************** */
+	
 	public void muscleDesc() {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-
-		// set title
-		//alertDialogBuilder.setTitle("Your Title");
-
 		// set dialog message;
 		alertDialogBuilder
 		.setMessage("Tone and build muscle mass utilizing different resistance tools.");
@@ -147,6 +137,11 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
+
+	/*************************************************************************
+	 * Shows the description of the BOOT CAMP class
+	 *************************************************************************** */
+	
 	public void bootDesc() {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -157,6 +152,11 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
+
+	/*************************************************************************
+	 * Shows the description of the YOGALETES class
+	 *************************************************************************** */
+	
 	public void yogaletesDesc() {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -167,6 +167,11 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
+
+	/*************************************************************************
+	 * Shows the description of the YOGA class
+	 *************************************************************************** */
+	
 	public void yogaDesc() {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -177,6 +182,10 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
+
+	/*************************************************************************
+	 * Shows the description of the interval training class
+	 *************************************************************************** */
 	
 	public void intervalDesc() {
 
@@ -188,6 +197,11 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
+
+	/*************************************************************************
+		Shows the description of the KICKBOXING class
+	 *************************************************************************** */
+	
 	public void kickDesc() {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -198,6 +212,11 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
+
+	/*************************************************************************
+	 * Shows the description of the Strength class
+	 *************************************************************************** */
+	
 	public void strengthDesc() {
 
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -208,5 +227,5 @@ public class GroupsActivity extends Activity {
 		// show it
 		alertDialog.show();
 	}
-	
+
 }
--- src/edu/ggc/it/gym/GymMainActivity.java
+++ src/edu/ggc/it/gym/GymMainActivity.java
@@ -1,88 +1,144 @@
 package edu.ggc.it.gym;
 
-import edu.ggc.it.Main;
-import edu.ggc.it.R;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import edu.ggc.it.R;
 
 public class GymMainActivity extends Activity {
 
-	
-		private Button contract;
-		private Button schedule;
-		private Button groups;
-		private Button buddiesButton;
-		private Button magazine;
-		private Button HomeButton;
-		private Context myContext;
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_gym_main);
-			
-			myContext = this;
-			contract = (Button) findViewById(R.id.firstTime);
-			contract.setOnClickListener(new ButtonListener());
-			
-			schedule = (Button) findViewById(R.id.gymSchedule);
-			schedule.setOnClickListener(new ButtonListener());
-			
-			groups = (Button) findViewById(R.id.Group);
-			groups.setOnClickListener(new ButtonListener());
-			
-			buddiesButton = (Button) findViewById(R.id.buddies);
-			buddiesButton.setOnClickListener(new ButtonListener());
-			
-			magazine = (Button) findViewById(R.id.healthMagazine);
-			magazine.setOnClickListener(new ButtonListener());
-			
-			HomeButton = (Button) findViewById(R.id.homeButton);
-			HomeButton.setOnClickListener(new ButtonListener());
-			
-		}
 
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			// Inflate the menu; this adds items to the action bar if it is present.
-			getMenuInflater().inflate(R.menu.activity_gym_main, menu);
-			return true;
-		}
+	private Button contract;
+	private Button schedule;
+	private Button groups;
+	private Button buddiesButton;
+	private Button magazine;
+	private Button HomeButton;
+	private Context myContext;
+	private TextView quote;
+	private Intent intent;
+	public final static String EXTRA_MESSAGE = "edu.ggc.it.directory.MESSAGE";
+	@Override
+
+	/**
+	 * This method creates all of the buttons according to their names and 
+	 * locations of the button
+	 */
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gym_main);
 		
-		public class ButtonListener implements OnClickListener {
-			public void onClick(View view) {
-				
-				if (contract.isPressed()){
-					
-				}
-				else if (view.getId() == R.id.gymSchedule){
-					startActivity(new Intent (myContext, GymScheduleActivity.class));
-					//Toast.makeText(myContext, "What a cute cat!", Toast.LENGTH_LONG)
-					//.show();
-				}
-				else if (groups.isPressed()){
-					startActivity( new Intent ( myContext, GroupsActivity.class));
-				}
-				else if (view.getId() == R.id.buddies){
-					//startActivity(new Intent (myContext, WorkoutBuddies.class));
-			Toast.makeText(myContext, "What a cute cat!", Toast.LENGTH_LONG)
-					.show();
-				}
-				else if (magazine.isPressed()){
-					
-				}
-				else if (view.getId() == R.id.homeButton) {
-					startActivity(new Intent (myContext, Main.class));
-				}
-			}
+		myContext = this;
+		contract = (Button) findViewById(R.id.firstTime);
+		contract.setOnClickListener(new ButtonListener());
 
+		schedule = (Button) findViewById(R.id.gymSchedule);
+		schedule.setOnClickListener(new ButtonListener());
+
+		groups = (Button) findViewById(R.id.Group);
+		groups.setOnClickListener(new ButtonListener());
+
+		buddiesButton = (Button) findViewById(R.id.buddies);
+		buddiesButton.setOnClickListener(new ButtonListener());
+
+		magazine = (Button) findViewById(R.id.healthMagazine);
+		magazine.setOnClickListener(new ButtonListener());
+
+		quote = (TextView) findViewById(R.id.quoteTextView);
+
+		HomeButton = (Button) findViewById(R.id.homeButton);
+		HomeButton.setOnClickListener(new ButtonListener());
+		
+		
+		
+		try {
+			// insert text file in the assets folder
+			// you can't write to the assests folder, it is read-only
+
+			AssetManager am = myContext.getAssets();
+			InputStream in = am.open("quotes.txt");			
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(in));
+			String str;
+			ArrayList <String> quotes = new ArrayList<String>();
+			while ((str = reader.readLine()) != null) {
+				quotes.add(str);
+				Collections.shuffle(quotes);
+			}
+			in.close();
+			
+			quote.setText(quotes.get(0).toString());
+			//in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_gym_main, menu);
+		return true;
+	}
+
+
+	/**
+	 * this method creates the listeners for all of the buttons individually
+	 * 
+	 */
+	public class ButtonListener implements OnClickListener {
+		public void onClick(View view) {
+
+			if (contract.isPressed()){
+
+			}
+			else if (view.getId() == R.id.gymSchedule){
+				startActivity(new Intent (myContext, GymScheduleActivity.class));
+				//Toast.makeText(myContext, "What a cute cat!", Toast.LENGTH_LONG)
+				//.show();
+			}
+			else if (groups.isPressed()){
+				startActivity( new Intent ( myContext, GroupsActivity.class));
+			}
+			else if (view.getId() == R.id.buddies){
+				//startActivity(new Intent (myContext, WorkoutBuddies.class));
+				startActivity(new Intent(myContext, Calendar_gym.class));
+			}
+			else if (magazine.isPressed()){
+				startActivity(new Intent(myContext, MagazineActivity.class));
+				String Url;
+				 Url = "http://readsh101.com/ggc.html";
+        		 intent.putExtra(EXTRA_MESSAGE, Url);
+        		 startActivity(intent);
+			}
+			else if (view.getId() == R.id.homeButton) {
+				finish();
+			}
 		}
 
 	}
+
+
+	
+	
+
+
+}
