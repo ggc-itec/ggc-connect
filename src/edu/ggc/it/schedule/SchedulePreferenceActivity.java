8ea89caed9f67ea99a638713d157faabb899c2aa
rramsaro <rramsaro@ggc.edu>
2013-04-17 05:54:14 -0400

    Added setting for reminder of class start time...

--- src/edu/ggc/it/schedule/SchedulePreferenceActivity.java
+++ src/edu/ggc/it/schedule/SchedulePreferenceActivity.java
@@ -5,13 +5,36 @@ import edu.ggc.it.R.layout;
 import edu.ggc.it.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
+import android.widget.Spinner;
 
 public class SchedulePreferenceActivity extends Activity {
+	
+	private Spinner spnReminderTime;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.schedule_preference);
+		
+		spnReminderTime = (Spinner) findViewById(R.id.schedule_pref_spn_reminder);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SharedPreferences settings = getPreferences(0);
+		spnReminderTime.setSelection(settings.getInt("schedule_reminder_pos", 0));
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		SharedPreferences settings = getPreferences(0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putString("schedule_reminder_string", spnReminderTime.getSelectedItem().toString());
+		editor.putInt("schedule_reminder_pos", spnReminderTime.getSelectedItemPosition());
+		editor.commit();
 	}
 }
