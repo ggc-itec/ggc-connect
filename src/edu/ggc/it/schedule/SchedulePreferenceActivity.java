2e3e461cf05655b92d910f67d0d4bb367c22d3ca
rramsaro <rramsaro@ggc.edu>
2013-04-17 05:32:42 -0400

    Changed layout of sched. pref screen

---
+++ src/edu/ggc/it/schedule/SchedulePreferenceActivity.java
@@ -0,0 +1,17 @@
+package edu.ggc.it.schedule;
+
+import edu.ggc.it.R;
+import edu.ggc.it.R.layout;
+import edu.ggc.it.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SchedulePreferenceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.schedule_preference);
+	}
+}
