a17db353a8130338288f0a4d6702cc051a79d63b
jordan-r-smith <jordan.r.smith@gmail.com>
2013-11-24 18:42:41 -0500

    Finishing the project

--- src/edu/ggc/it/gpacalc/GPACalcSemester.java
+++ src/edu/ggc/it/gpacalc/GPACalcSemester.java
@@ -1,283 +1,289 @@
 package edu.ggc.it.gpacalc;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import edu.ggc.it.R;
 
+/**
+ * @author Kyle Dornblaser
+ * @author Jordan Smith
+ * @author Timothy McCravy
+ * 
+ * This activity gets the user's current course credit hours and the estimated grade for the
+ * current semester and displays the GPA in a dialog.
+ */
+
 public class GPACalcSemester extends Activity
 {
-	// List of all the info on as many classes as the user inputs
-	// First is the creditHours info, then the Grade info: they are always
-	// grouped together by the class they refer to
-	private ArrayList<Spinner> classInfoList = new ArrayList<Spinner>();
+    //Holds the creditHours spinners
+    private ArrayList<Spinner> creditHoursList = new ArrayList<Spinner>();
+    //Holds the grades spinners
+    private ArrayList<Spinner> gradesList = new ArrayList<Spinner>();
+    //Holds the dynamically generated LinearLayouts
+    private ArrayList<LinearLayout> childLayoutList = new ArrayList<LinearLayout>();
 
-	// How many classes are currently on-screen
-	private int totalClasses = 0;
+    private Button submitButton;
+    private Button addCourseButton;
+    private Button removeCourseButton;
 
-	// The text that will show the new GPA after it is calculated
-	private TextView newGPATextView;
+    //The hours and GPA received from the previous activity: GPACalcActivity
+    //They represent the previous hours and GPA of the user before this semester
+    private int previousHours;
+    private float previousGPA;
 
-	// All the buttons
-	private Button submitButton;
-	private Button addAClassButton;
-	private Button removeAClassButton;
+    //Layout that can be modified
+    private LinearLayout semester_layout;
 
-	// The hours and GPA recieved from the previous activity: GPACalcActivity
-	// They represent the previous hours and GPA of the user before this
-	// semester
-	private int previousHours;
-	private float previousGPA;
-	
-	private LinearLayout ll;
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
+        //Receive data from previous activity GPACalcActivity
+        processIntent();
 
-		// Create the screen layout
-		//setContentView(R.layout.activity_gpacalc_semester);
+        //Create the screen layout
+        setContentView(R.layout.activity_gpacalc_semester);
+        semester_layout = (LinearLayout) findViewById(R.id.semester_layout);
 
-		// Receive data from previous activity GPACalcActivity
-		processIntent();
+        //Initial Class
+        addCourse();
 
-		// Add two classes (class as in schooling, not as in programming) and
-		// their spinners to the screen
-		//addAClass();
-		//addAClass();
+        //Clicking the button adds a course
+        addCourseButton = (Button) findViewById(R.id.button_add_course);
+        addCourseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                addCourse();
+            }
+        });
 
-		ScrollView sv = new ScrollView(this);
-		ll = new LinearLayout(this);
-		ll.setOrientation(LinearLayout.VERTICAL);
-		sv.addView(ll);
-		addAClass();
-		this.setContentView(sv);
+        //Removes the most recent course added when clicked
+        removeCourseButton = (Button) findViewById(R.id.button_remove_course);
+        removeCourseButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //Cannot remove the last course
+                if (childLayoutList.size() > 1)
+                {
+                    removeCourse();
+                }
+            }
+        });
 
-		// Add a button that can be pressed to add another class (class as in
-		// schooling, not as in programming)
-		/*addAClassButton = (Button) findViewById(R.id.button_add_class);
-		addAClassButton.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				addAClass();
-			}
-		});*/
+        //calculates the GPA and shows it when clicked
+        submitButton = (Button) findViewById(R.id.button_submit);
+        submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                displayGPA();
+            }
+        });
+    }
 
-		// Add a button that can be pressed to remove the most recent
-		// (bottom-most when looking at the screen) class (class as in
-		// schooling, not as in programming)
-		// The button is invisible when there is only one class remaining
-		/*removeAClassButton = (Button) findViewById(R.id.button_remove_class);
-		removeAClassButton.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				removeAClass();
-			}
-		});*/
+    /**
+     * Adds a course to the screen
+     */
+    private void addCourse()
+    {
+        //Adds a new LinearLayout to the ArrayList to be used by each course. This puts each course on its own line.
+        childLayoutList.add(new LinearLayout(this));
+        childLayoutList.get(childLayoutList.size()-1).setOrientation(LinearLayout.HORIZONTAL);
 
-		// Add the button that when pressed calculates the GPA and shows it to
-		// the user
-		/*newGPATextView = (TextView) findViewById(R.id.text_new_gpa);
-		submitButton = (Button) findViewById(R.id.button_submit);
-		submitButton.setOnClickListener(new View.OnClickListener()
-		{
-			@Override
-			public void onClick(View view)
-			{
-				newGPATextView.setText(Float.toString(calculateGPA()));
-			}
-		});*/
-	}
+        //Add the spinners to the related ArrayLists
+        creditHoursList.add(createCreditHoursSpinner());
+        gradesList.add(createGradeSpinner());
 
-	/**
-	 * Method: addAClass * adds a new class - school class, not programming
-	 * class - to the screen. The class has two spinners: credit hours and
-	 * grade. The user is to choose from the drop-down menu the correct option.
-	 */
-	private void addAClass()
-	{
+        //Add TextViews and Spinners to the LinearLayout ArrayList
+        childLayoutList.get(childLayoutList.size()-1).addView(createTextView("Credit Hours"));
+        childLayoutList.get(childLayoutList.size()-1).addView(creditHoursList.get(creditHoursList.size()-1));
+        childLayoutList.get(childLayoutList.size()-1).addView(createTextView("Expected Grade"));
+        childLayoutList.get(childLayoutList.size()-1).addView(gradesList.get(gradesList.size()-1));
 
-		// Add the spinners to the class info list
-		classInfoList.add(createCreditHoursSpinner());
-		classInfoList.add(createGradeSpinner());
+        //Add the LinearLayout to the screen
+        semester_layout.addView(childLayoutList.get(childLayoutList.size()-1));
+    }
 
-		// Increment total classes number by one
-		totalClasses++;
-	}
+    /**
+     * Removes the most recent course added to the screen
+     */
+    private void removeCourse()
+    {
+        //Remove the spinners from the related ArrayLists
+        creditHoursList.remove(creditHoursList.size()-1);
+        gradesList.remove(gradesList.size()-1);
 
-	/**
-	 * Method: removeAClass * Removes the bottom-most - the most recently
-	 * created - class'es spinners from both the screen and the class info list.
-	 * Can only be called when there are at least two classes.
-	 */
-	private void removeAClass()
-	{
-		// Get the location of the most recently created class'es second spinner
-		int spinnerLocation = classInfoList.size() - 1;
+        //Removes the course from the screen
+        semester_layout.removeView(childLayoutList.get(childLayoutList.size()-1));
+        childLayoutList.remove(childLayoutList.size()-1);
+    }
 
-		// Remove the spinners from the class info list
-		Spinner creditHourSpinnerToRemove = classInfoList
-				.remove(spinnerLocation);
-		Spinner gradeSpinnerToRemove = classInfoList
-				.remove(spinnerLocation - 1); // The grade spinner is always
-		// right before the creditHour
-		// spinner
+    /**
+     * Returns a TextView that contains the passed through string.
+     * 
+     * @param  text  will display in the TextView
+     * @return 		 a TextView with the passed through string
+     */
+    private TextView createTextView(String text)
+    {
+        TextView creditHoursTextView = new TextView(this);
+        creditHoursTextView.setText(text + ": ");
+        
+        return creditHoursTextView;
+    }
 
-		// Decrement total classes number by one
-		totalClasses--;
-	}
+    /**
+     * Returns a Spinner that contains the choices A, B, C, D, and F
+     * 
+     * @return  the created grade spinner
+     */
+    private Spinner createGradeSpinner()
+    {
+        //Create the grade spinner
+        Spinner gradeSpinner = new Spinner(this);
 
-	/**
-	 * Method: createGradeSpinner * creates a grade spinner with the options: A,
-	 * B, C, D, and F. The options are Strings, not Char(acter)s.
-	 * 
-	 * @return gradeSpinner the created grade spinner
-	 */
-	private Spinner createGradeSpinner()
-	{
-		// Create the grade spinner
-		//Spinner gradeSpinner = (Spinner) findViewById(R.id.input_class_one_grade);
+        //Create all the options for the grade spinner
+        List<String> gradesList = new ArrayList<String>();
+        gradesList.add("A");
+        gradesList.add("B");
+        gradesList.add("C");
+        gradesList.add("D");
+        gradesList.add("F");
 
-		Spinner gradeSpinner = new Spinner(this);
-		ll.addView(gradeSpinner);
+        //Attach the grade list to the spinner with an adapter
+        ArrayAdapter<String> gradesSpinnerAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, gradesList);
+        gradesSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gradeSpinner.setAdapter(gradesSpinnerAdapter);
 
-		// Create all the options for the grade spinner
-		List<String> gradesList = new ArrayList<String>();
-		gradesList.add("A");
-		gradesList.add("B");
-		gradesList.add("C");
-		gradesList.add("D");
-		gradesList.add("F");
+        //Return the created spinner
+        return gradeSpinner;
+    }
 
-		// Attach the grade list to the spinner with an adapter
-		ArrayAdapter<String> gradesSpinnerAdapter = new ArrayAdapter<String>(
-				this, android.R.layout.simple_spinner_item, gradesList);
+    /**
+     * Returns a Spinner that contains the choices 1, 2, 3, 4
+     * 
+     * @return  the created credit hours spinner
+     */
+    private Spinner createCreditHoursSpinner()
+    {
+        //Create the credit hours spinner
+        Spinner creditHoursSpinner = new Spinner(this);
 
-		gradesSpinnerAdapter
-		.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Create all the options for the credit hours spinner
+        List<String> creditHoursList = new ArrayList<String>();
+        creditHoursList.add("1");
+        creditHoursList.add("2");
+        creditHoursList.add("3");
+        creditHoursList.add("4");
 
-		gradeSpinner.setAdapter(gradesSpinnerAdapter);
+        //Attach the credit hours list to the spinner with an adapter
+        ArrayAdapter<String> creditHoursSpinnerAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, creditHoursList);
+        creditHoursSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        creditHoursSpinner.setAdapter(creditHoursSpinnerAdapter);
 
-		// Return the created spinner
-		return gradeSpinner;
-	}
+        //Return the created credit hours spinner
+        return creditHoursSpinner;
+    }
 
-	/**
-	 * Method: createCreditHoursSpinner * creates a credit hours spinner with
-	 * the options: 1, 2, 3, and 4. The options are Strings, not int(eger)s.
-	 * 
-	 * @return creditHoursSpinner the created credit hours spinner
-	 */
-	private Spinner createCreditHoursSpinner()
-	{
-		// Create the credit hours spinner
-		//Spinner creditHoursSpinner = (Spinner) findViewById(R.id.input_class_one_hours);
-		Spinner creditHoursSpinner = new Spinner(this);
-		ll.addView(creditHoursSpinner);
+    /**
+     * Retrieve the intent from the previous activity
+     */
+    private void processIntent()
+    {
+        Intent receivedIntent = getIntent();
+        previousGPA = receivedIntent.getFloatExtra("gpa", 0);
+        previousHours = receivedIntent.getIntExtra("hours", 0);
+    }
 
-		// Create all the options for the credit hours spinner
-		List<String> creditHoursList = new ArrayList<String>();
-		creditHoursList.add("1");
-		creditHoursList.add("2");
-		creditHoursList.add("3");
-		creditHoursList.add("4");
+    /**
+     * Converts the passed through String letter grade into a float number on the 4.0 GPA scale 
+     * 
+     * @param  letter  the grade letter which is being converted
+     * @return         the grade as a float on 4.0 GPA scale
+     */
+    private float convertGradeLetter(String letter)
+    {
+        if (letter.equals("A"))
+            return 4.0f;
+        else if (letter.equals("B"))
+            return 3.0f;
+        else if (letter.equals("C"))
+            return 2.0f;
+        else if (letter.equals("D"))
+            return 1.0f;
+        else
+            return 0.0f;
+    }
 
-		// Attach the credit hours list to the spinner with an adapter
-		ArrayAdapter<String> creditHoursSpinnerAdapter = new ArrayAdapter<String>(
-				this, android.R.layout.simple_spinner_item, creditHoursList);
+    /**
+     * Build an AlertDialog box and display the calculated GPA in it
+     */
+    private void displayGPA()
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Calculated GPA");
+        DecimalFormat df = new DecimalFormat("#.##");
+        alert.setMessage(df.format(calculateGPA()));
 
-		creditHoursSpinnerAdapter
-		.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        alert.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {}
+        });
 
-		creditHoursSpinner.setAdapter(creditHoursSpinnerAdapter);
+        alert.show();
+    }
 
-		// Return the created credit hours spinner
-		return creditHoursSpinner;
-	}
+    /**
+     * Takes all the inputed values from the spinners and
+     * the previous GPA and hours and uses them to calculate the new GPA. Note:
+     * this is basically the end of the program. Called when the submitButton is
+     * pressed.
+     * 
+     * @return the new GPA
+     */
+    private float calculateGPA()
+    {
+        //Initialize the totals and input the previous GPA and hours to them
+        float totalPoints = previousGPA * previousHours;
+        float totalHours = previousHours;
 
-	private void processIntent()
-	{
-		Intent receivedIntent = getIntent();
-		previousGPA = receivedIntent.getFloatExtra("gpa", 0);
-		previousHours = receivedIntent.getIntExtra("hours", 0);
-	}
+        //Go through the classInfo list and add each course's values to the totals
+        for (int i = 0; i < creditHoursList.size() && i < gradesList.size(); i++)
+        {
+            //Get the course hours and convert it to float
+            float hours = Float.parseFloat(creditHoursList.get(i).getSelectedItem().toString());
 
-	/**
-	 * Method: convertGradeLetter * converts a grade letter into its
-	 * corresponding GPA number. A becomes 4.0, C becomes 2.0, F becomes 0.0,
-	 * ect.
-	 * 
-	 * @param letter
-	 *            the grade letter which is being converted
-	 * @return the grade as a float number
-	 */
-	private float convertGradeLetter(String letter)
-	{
-		if (letter.equals("A"))
-			return 4.0f;
-		else if (letter.equals("B"))
-			return 3.0f;
-		else if (letter.equals("C"))
-			return 2.0f;
-		else if (letter.equals("D"))
-			return 1.0f;
-		else if (letter.equals("F"))
-			return 0.0f;
-		else
-			return -1; // THIS IS AN ERROR. SOMBODY NEEDS TO MAKE THIS THROW AN
-		// EXCEPTION/ASSERT.
-	}
+            //Get the course grade
+            float grade = convertGradeLetter(gradesList.get(i).getSelectedItem().toString());
 
-	/**
-	 * Method: calculateGPA * takes all the inputed values from the spinners and
-	 * the previous GPA and hours and uses them to calculate the new GPA. Note:
-	 * this is basically the end of the program. Called when the submitButton is
-	 * pressed.
-	 * 
-	 * @return the new GPA
-	 */
-	private float calculateGPA()
-	{
-		// Initialize the totals and input the previous GPA and hours to them
-		float totalPoints = previousGPA * previousHours;
-		float totalHours = previousHours;
+            //Get the course points using the hours and grade
+            float points = hours * grade;
 
-		// Go through the classInfo list and add each class'es values to the
-		// totals
-		while (classInfoList.size() > 1)
-		{
-			// Get the class'es hours and convert it to float
-			float hours = Float.parseFloat(classInfoList.remove(0)
-					.getSelectedItem().toString());
+            //Add the hours and points to the totals
+            totalHours += hours;
+            totalPoints += points;
+        }
 
-			// Get the class'es grade
-			float grade = convertGradeLetter(classInfoList.remove(1)
-					.getSelectedItem().toString());
-
-			// Get the class'es points using the hours and grade
-			float points = hours * grade;
-
-			// Add the hours and points to the totals
-			totalHours += hours;
-			totalPoints += points;
-		}
-
-		// Return the GPA using totalPoints and totalHours
-		return totalPoints / totalHours;
-	}
+        //Return the GPA using totalPoints and totalHours
+        return totalPoints / totalHours;
+    }
 }
